f27de2567777044c72dd11f2a12a275d59a09550
Nimish Gupta <nimish28@gmail.com>
2014-02-07 03:06:09 -0500

    a little more robust command execution. stderr and stdout connected via network

--- src/main/scala/CookServer.scala
+++ src/main/scala/CookServer.scala
@@ -10,13 +10,45 @@ import spray.http.ContentType._
 import HttpMethods._
 import MediaTypes._
 
+
+import java.io.File
+import java.io.FileOutputStream
+import scala.sys.process._
+
 class CookService extends Actor {
 
   implicit val timeout: Timeout = 1.second // for the actor 'asks'
   import context.dispatcher // ExecutionContext for the futures and scheduler
 
+  val newline = sys.props ("line.separator")
+
   def execute_command (cmd: String): String = {
-    "Executing cmd: " + cmd
+    // Create a temporary file
+    try {
+      val file = File.createTempFile ("cookpre", ".tmp")
+      file.setExecutable (true)
+
+      // Write out our cmd
+      val out = new FileOutputStream (file)
+      out.getChannel ().force (true)
+      out.write (cmd.getBytes)
+      out.close ()
+
+      var outlog, errlog = ""
+
+      var logger = ProcessLogger ((s) => outlog += (s + newline),
+                                  (s) => errlog += (s + newline))
+
+      val status: Int = (file.getCanonicalPath ()) ! logger
+
+      // Done with file delete
+      file.delete ()
+
+      outlog + errlog
+    } catch {
+      case _: java.io.IOException => ""
+    }
+
   }
 
   def receive = {
