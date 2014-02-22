b0080492d99dd6415d3bcd715866c8cb497ff57f
Nimish Gupta <nimish28@gmail.com>
2014-02-22 00:54:07 -0500

    working bootstrap script for setting up jvm and scala

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
-apt-get install openjdk-7-jdk openjdk-7-jre scala
+apt-get update
+apt-get install -y vim openjdk-7-jdk openjdk-7-jre scala curl
 wget http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
 dpkg -i sbt.deb
