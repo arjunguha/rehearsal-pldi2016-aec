9ddc2560e775ba29948ecd57effa9eb34332e366
Nimish Gupta <nimish28@gmail.com>
2014-02-22 00:09:14 -0500

    Abstraction for apt-get packages introduced

--- src/main/scala/drivers/Apply2.scala
+++ src/main/scala/drivers/Apply2.scala
@@ -1,11 +1,9 @@
 /* TODO : Add update and uninstall support */
 /* TODO : Batching of install, doing an independent apt-get install every time is painfull */
 
-
 trait Command {
 
   val cmd_name: String
-
   def exec (args: String): Int = (Cmd.exec (cmd_name + " " + args)._1)
 }
 
@@ -23,8 +21,8 @@ trait Service {
 
 
 sealed trait Package {
-  val package_name: String
-  val package_desc: String = ""
+  val pkg_name: String
+  val pkg_desc: String = ""
 
   def preinstall (): Int = 0
   def install (): Int // Mandatory
@@ -36,30 +34,41 @@ sealed trait Package {
 trait LocalPackage extends Package {
 
   override def install (/* TODO : Configuration */): Int = 
-    (Cmd.exec ("apt-get install -q -y" + " " + package_name))._1
+    (Cmd.exec ("apt-get install -q -y" + " " + pkg_name))._1
 }
 
-trait NonLocalPackage extends Package {}
+
+trait NonLocalPackage extends Package {
+
+  // TODO : make stage absract here and let NonLocalPackages provide a definition
+  def stage (): Int = 0
+}
+
+
+class CommandLocalPackage (_pkg_name: String,
+                           _cmd_name: String = "",
+                           _pkg_desc: String = "") extends LocalPackage
+                                                   with Command {
+  override val pkg_name = _pkg_name
+  override val cmd_name = if (_cmd_name == "") _pkg_name else _cmd_name
+  override val pkg_desc = _pkg_desc
+}
+
 
 
 
 /* TODO : Need to make these singleton objects */
 
-
-/* Make encompasses autoconf automake .. */
-class Make extends LocalPackage
-           with Command { 
-  override val package_name = "make"
-  override val cmd_name = package_name
+class DebconfUtils extends LocalPackage {
+  override val pkg_name = "debconf-utils"
 }
-
 
 
 class CouchDB (val host: String = "localhost",
                val port: String = "5894") extends LocalPackage
                                           with Service {
-  override val package_name = "couchdb"
-  override val svc_name = package_name
+  override val pkg_name = "couchdb"
+  override val svc_name = pkg_name
 
   override def postinstall () = {
 
@@ -78,45 +87,23 @@ class CouchDB (val host: String = "localhost",
 
 
 
-class Mercurial extends LocalPackage
-                with Command {
-  override val package_name = "mercurial"
-  override val cmd_name = "hg"
-}
-
-class Wget extends LocalPackage
-                   with Command {
-  override val package_name = "wget"
-  override val cmd_name = package_name
-}
-
-class Tar extends LocalPackage with Command {
-  override val package_name = "tar"
-  override val cmd_name = package_name
-}
-
-class CPP extends LocalPackage with Command {
-  override val package_name = "g++"
-  override val cmd_name = package_name
-}
-
-class Node (wget: Wget,
-            tar: Tar,
-            make: Make,
-            cppc: CPP) extends NonLocalPackage
-                        with Command {
-  override val package_name = "node"
-  override val cmd_name = package_name
+class Node (wget: Command,
+            tar:  Command,
+            make: Command,
+            cppc: Command) extends NonLocalPackage
+                           with Command {
+  override val pkg_name = "node"
+  override val cmd_name = pkg_name
 
   val version = "v0.10.25"
 
   val src = "http://nodejs.org/dist/" + version + "/" + 
-            package_name + "-" + version + ".tar.gz"
+            pkg_name + "-" + version + ".tar.gz"
 
   override def install (): Int = {
     wget.exec (src)
-    tar.exec ("-xzf" + " " + package_name + "-" + version + ".tar.gz")
-    cd (package_name + "-" + version)
+    tar.exec ("-xzf" + " " + pkg_name + "-" + version + ".tar.gz")
+    cd (pkg_name + "-" + version)
       Cmd.exec ("./configure")
       make.exec ("")
       make.exec ("install")
@@ -130,8 +117,8 @@ class Node (wget: Wget,
 class Npm (node: Node) extends NonLocalPackage
                        with Command {
 
-  override val package_name = "npm"
-  override val cmd_name = package_name
+  override val pkg_name = "npm"
+  override val cmd_name = pkg_name
 
   override def install () = 0
 }
@@ -142,7 +129,7 @@ class Npm (node: Node) extends NonLocalPackage
  */
 class TypeScript (npm: Npm) extends NonLocalPackage
                             with Command {
-  override val package_name = "typescript"
+  override val pkg_name = "typescript"
   override val cmd_name = "tsc"
 
   override def install (): Int = 
@@ -150,21 +137,11 @@ class TypeScript (npm: Npm) extends NonLocalPackage
 }
 
 
-class Nginx extends LocalPackage
-            with Command {
-  override val package_name = "nginx"
-  override val cmd_name = package_name
-}
 
 
-class DebconfUtils extends LocalPackage {
-  override val package_name = "debconf-utils"
-}
-
-
-class GoLang (debconfutils: DebconfUtils) extends LocalPackage
+class GoLang (debconfutils: Package) extends LocalPackage
                                           with Command {
-  override val package_name = "golang"
+  override val pkg_name = "golang"
   override val cmd_name = "go"
 
   override def preinstall (): Int = {
@@ -173,30 +150,23 @@ class GoLang (debconfutils: DebconfUtils) extends LocalPackage
 }
 
 
-class Git extends LocalPackage 
-          with    Command {
-
-  override val package_name = "git"
-  override val cmd_name = package_name
-}
 
 
-class Apply2 (make    : Make,
-              golang  : GoLang,
+class Apply2 (make    : Command,
+              golang  : Package,
               couchdb : CouchDB, /* XXX : External dependency */
-              hg      : Mercurial,
-              nginx   : Nginx,
-              git     : Git,
+              hg      : Command,
+              nginx   : Package,
+              git     : Command,
               ts      : TypeScript) extends NonLocalPackage
                                     with Command
                                     with Service {
 
 
-  override val package_name = "Apply2"
+  override val pkg_name = "Apply2"
   override val cmd_name = "apply2"
   override val svc_name = cmd_name
-  override val package_desc = "Admission system for UMass"
-
+  override val pkg_desc = "Admission system for UMass"
 
   val repo_loc = "https://github.com/nimishgupta/Apply2.git"
 
@@ -242,22 +212,22 @@ object Main {
   def main (args: Array[String]) {
 
     /* TODO : Should have been RAII */
-    val make = new Make
+    val make = new CommandLocalPackage ("make")
     install (make)
 
     val couchdb = new CouchDB
     install (couchdb)
 
-    val hg = new Mercurial
+    val hg = new CommandLocalPackage ("mercurial", "hg")
     install (hg)
 
-    val wget = new Wget
+    val wget = new CommandLocalPackage ("wget")
     install (wget)
 
-    val tar = new Tar
+    val tar = new CommandLocalPackage ("tar")
     install (tar)
 
-    val gcppc = new CPP
+    val gcppc = new CommandLocalPackage ("g++")
     install (gcppc)
 
     val node = new Node (wget, tar, make, gcppc)
@@ -269,7 +239,7 @@ object Main {
     val tsc = new TypeScript (npm)
     install (tsc)
 
-    val nginx = new Nginx
+    val nginx = new CommandLocalPackage ("nginx")
     install (nginx)
 
     val debconfutils = new DebconfUtils
@@ -278,7 +248,7 @@ object Main {
     val go = new GoLang (debconfutils)
     install (go)
 
-    val git = new Git
+    val git = new CommandLocalPackage ("git")
     install (git)
 
     val apply2 = new Apply2 (make, go, couchdb, hg, nginx, git, tsc)
