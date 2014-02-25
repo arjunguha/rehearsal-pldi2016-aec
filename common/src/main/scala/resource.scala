d135cfa70cc16e9c547c1607ef7c76e7f8e28cb4
Nimish Gupta <nimish28@gmail.com>
2014-02-25 13:42:53 -0500

    Using shapeless to accomodate heterogenous types

--- common/src/main/scala/resource.scala
+++ common/src/main/scala/resource.scala
@@ -14,32 +14,48 @@
 /* But how to link generic shell script execution with command line parameters that can be derived from properties */
 
 
+import shapeless._
+import poly._
+
+
 trait Prop [T] {
 
   def v () : T
 }
 
 
-class StaticProp [T] (private val v: T) extends Prop [T] {
+class StaticProp [T] (private val _v: T) extends Prop [T] {
 
-  def v () = v
+  def v () = _v
 }
 
 
 class DynamicProp [T] (private val eval_script: String,
-                       private val fallback: T) extends Prop [T] {
+                       private val fallback: T,
+                       /* TODO : Accepting function for now to compile */
+                       private val f: (String => T)) extends Prop [T] {
 
   def v () : T = {
-    
-    // TODO : Implicit conversion
-    Cmd.exec (eval_script)
+
+    (Cmd.exec (eval_script)) match {
+      case (0, out, _) => f (out) /* TODO: implicit conversion to T, may restrict the kinds to T as any code cannot be translated over network */
+      case (_, _, err) => fallback
+    }
   }
 }
 
 
-class Resource (name: String, 
-                install_method: /*Native/Custom*/,
-                deps: Resource*) {
+class Resource (val name: String, 
+                val install_method: Int /* Native / Custom */,
+                val ImportConfigs: HList /* of Prop */,
+                val ExportConfigs: HList /* of Prop */,
+                val deps: Resource*) {
 
+  /* TODO : Consruct a map out of deps */  
+  /* TODO : Construct a map of Configs both import and export */
 
+  def install () : Int = 0
+
+  /* TODO : Poly Type */
+  def get_prop (name: String) : String = "missing"
 }
