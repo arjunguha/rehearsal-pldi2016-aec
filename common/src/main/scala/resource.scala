7e0c8119b23ac8c79a5c40510a0b79a3f3cc3036
Nimish Gupta <nimish28@gmail.com>
2014-02-26 00:33:23 -0500

    Installation methods defined, case classes for Ports and Install Methods declared

--- common/src/main/scala/resource.scala
+++ common/src/main/scala/resource.scala
@@ -1,4 +1,4 @@
-/* Needs to be transferrable over network using an actor */
+/* TODO : Check that everythings Needs to be transferrable over network using an actor, that is reconstructable as it is over network */
 /* Generics across network!! */
 
 /* resource can be installed natively or customly */
@@ -15,47 +15,83 @@
 
 
 import shapeless._
+import record._
+import syntax.singleton._
 import poly._
+import HList._
 
 
-trait Prop [T] {
+/* Prop ADT */
+sealed trait Prop [+T] { def v () : T }
+case class StaticProp [T] (private val _v: T) extends Prop [T] { def v () = _v }
 
-  def v () : T
-}
-
-
-class StaticProp [T] (private val _v: T) extends Prop [T] {
-
-  def v () = _v
-}
-
-
-class DynamicProp [T] (private val eval_script: String,
-                       private val fallback: T,
-                       /* TODO : Accepting function for now to compile */
-                       private val f: (String => T)) extends Prop [T] {
+// Accepts file or command using which it evaluates the property
+case class DynamicProp [T] (private val eval_script: String,
+                            private val fallback: T,
+                            implicit val parse: (String => T)) extends Prop [T] {
 
   def v () : T = {
 
     (Cmd.exec (eval_script)) match {
-      case (0, out, _) => f (out) /* TODO: implicit conversion to T, may restrict the kinds to T as any code cannot be translated over network */
+      case (0, out, _) => parse (out) /* TODO: implicit conversion to T, may restrict the kinds to T as any code cannot be translated over network */
       case (_, _, err) => fallback
     }
   }
 }
 
+case class MappedProp [T] (private val p: Prop[T]) extends Prop [T] { def v (): T = p.v () }
+
+
+
+/////////////////////////////////////////////////////////////////////////
+sealed trait InstallMethod
+case object Native extends InstallMethod
+case class Custom (val script: String) extends InstallMethod
+
+
+
+object InstallResource {
+
+
+  def props_to_strlst[L <: HList, Prop[_]] (props: L) : Unit = {}
+
+  def apply[L <: HList, Prop[_]] (name: String, method: InstallMethod, props: L): Int = {
+    method match {
+      case Native => Cmd.exec ("apt-get install -q -y" + " " + name)._1
+      case Custom (script) => Cmd.exec (script)._1 /* TODO : Convert props list to string list */
+    }
+  }
+
+  def apply (name: String, method: InstallMethod): Int =
+    apply (name, method, HNil)
+}
+
+
+  
+
+
+/* TODO : Consruct a Global map out of deps, to answer certain queries */  
+
 
 class Resource (val name: String, 
-                val install_method: Int /* Native / Custom */,
-                val ImportConfigs: HList /* of Prop */,
-                val ExportConfigs: HList /* of Prop */,
+                val install_type: InstallMethod,
+                val iconfig: HList, /* TODO : Change to maps */ /* of Prop */
+                val econfig: HList, /* TODO : Change to maps */ /*of Prop */
                 val deps: Resource*) {
 
-  /* TODO : Consruct a map out of deps */  
-  /* TODO : Construct a map of Configs both import and export */
+  def install_deps (rs: Seq[Resource]) = {
 
-  def install () : Int = 0
+    rs.foreach (x => x.install ())
+  }
+
+  def install () : Int = {
+
+    install_deps (deps)
+    InstallResource (name, install_type, iconfig)
+  }
 
   /* TODO : Poly Type */
-  def get_prop (name: String) : String = "missing"
+  def get_prop (name: String) : String = {
+    "missing"
+  }
 }
