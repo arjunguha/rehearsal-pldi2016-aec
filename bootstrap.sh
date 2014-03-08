51c1a306a17cffcfe5fb1d9a80f2b9eb65b1f5d0
Nimish Gupta <nimish28@gmail.com>
2014-03-07 23:51:17 -0500

    couchdb not behaving well with scala process builder, assuming for now that it is running on target machine and we only need to configure it for our needs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install -y vim openjdk-7-jdk openjdk-7-jre scala curl
+apt-get install -y vim openjdk-7-jdk openjdk-7-jre scala curl couchdb
 wget http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
 dpkg -i sbt.deb
--- couchdb_setup.sh
+++ couchdb_setup.sh
@@ -1,11 +1,24 @@
 #! /bin/bash
 
-apt-get install -q -y couchdb
+curl -X PUT http://127.0.0.1:5984/_config/httpd/bind_address -d \"0.0.0.0\"
+curl -X PUT http://127.0.0.1:5984/_config/httpd/port -d \"$port\"
 
-cat <<__EOF__ > /etc/couchdb/local.ini
-[httpd]
-bind_address = \"$HOST\"
-port = \"$PORT\"
-__EOF__
+# curl -X PUT http://127.0.0.1:5984/_config/httpd/port -d \"$port\"
 
-service couchdb restart
+# cat <<__EOF__ > /etc/couchdb/local.ini
+# [httpd]
+# bind_address = 0.0.0.0
+# port = $port
+# __EOF__
+
+# service couchdb restart
+#curl -X put http://127.0.0.1:5984/_config/httpd/bind_address -d "$host"
+#curl -X put http://127.0.0.1:5984/_config/httpd/port -d "$port"
+
+# cat <<__EOF__ > /tmp/restart_couchdb.py
+# import os
+# os.system ("service couchdb restart")
+# __EOF__
+# python /tmp/restart_couchdb.py &
+
+# exit $?
