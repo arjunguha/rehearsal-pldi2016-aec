e87e8901fd1a31880171059a069da0ebf83f9cb2
Nimish Gupta <nimish28@gmail.com>
2014-03-08 00:53:49 -0500

    correction in launch parameters, single machine install working fine

--- apply2_setup.sh
+++ apply2_setup.sh
@@ -5,4 +5,4 @@ cd /tmp && git clone https://github.com/nimishgupta/Apply2.git && cd Apply2 && m
 # Create and run a sample department
 /tmp/Apply2/apply2 newdept sample
 /tmp/Apply2/apply2 newreviewer scooby redbull64 "Scooby Doo"
-/tmp/Apply2/apply2 -dbhost $host -dbport $port
+/tmp/Apply2/apply2 -dbhost $host -dbport $port testserver
