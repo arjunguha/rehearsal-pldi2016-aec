79f4dd1eafc9639881cd6dd9c40bb0ecb5e7512f
Nimish Gupta <nimish28@gmail.com>
2014-03-09 12:55:41 -0400

    ip can now be optionally provided on command line

--- agent/src/main/scala/Main.scala
+++ agent/src/main/scala/Main.scala
@@ -2,13 +2,28 @@ import akka.kernel.Bootable
 import akka.actor.{ActorSystem, Actor, Props, ActorRef}
 import com.typesafe.config.ConfigFactory
 
-class WorkerActorSystem () extends Bootable {
+class WorkerActorSystem (ip: Option[String]) extends Bootable {
 
-  val system = ActorSystem ("WorkerSys", ConfigFactory.load.getConfig("slave"))
+  val config = ip match {
+    case Some (ip) => ConfigFactory.parseString ("akka.remote.netty.tcp { hostname=" + ip + "}")
+                      .withFallback (ConfigFactory.load.getConfig ("agent"))
+    case None => ConfigFactory.load.getConfig ("agent")
+  }
 
-  def startup  = println("a worker system came online")
+  val system = ActorSystem ("WorkerSys", config)
+
+  def startup  = println("An agent system came online")
   def shutdown = system.shutdown()
 }
 
 
-object WorkerApp { def main(args: Array[String]) { new WorkerActorSystem () }}
+object WorkerApp { 
+  
+  def main(args: Array[String]) { 
+  
+    args match {
+      case Array (ip) => new WorkerActorSystem (Some (ip))
+      case _          => new WorkerActorSystem (None)
+    }
+  }
+}
