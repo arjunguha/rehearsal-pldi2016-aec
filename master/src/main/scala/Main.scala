c30fa652208f36e0f22bbd279138ef16988e9ec8
Nimish Gupta <nimish28@gmail.com>
2014-03-10 12:48:37 -0400

    refactoring

--- master/src/main/scala/Main.scala
+++ master/src/main/scala/Main.scala
@@ -1,15 +1,19 @@
 import akka.kernel.Bootable
-import akka.actor.{Address, ActorSystem, ActorRef, Deploy}
+import akka.actor.{Address, ActorSystem, ActorRef, Deploy, Props}
 import akka.remote.RemoteScope
 import com.typesafe.config.ConfigFactory
 import akka.pattern.ask
+import scala.collection._
 import scala.concurrent._
 import scala.concurrent.duration._
 import akka.util.Timeout
 
 
 /* Why bootable */
-class MasterSystem (config: ResourceDesc) extends Bootable {
+class MasterSystem (config: ResourceDesc, 
+                    getActorProps: ((String, InstallMethod,
+                                     Map[String, String],
+                                     Map[String, ActorRef]) => Props)) extends Bootable {
 
   val system = ActorSystem ("Master", ConfigFactory.load.getConfig ("master"))
 
@@ -31,9 +35,10 @@ class MasterSystem (config: ResourceDesc) extends Bootable {
 
     val dep_map = res.deps.map (install_resource (_, Remote (install_ip))) toMap
     
+    // TODO : Flat vs heirarchical?
     val remote_addr = Address ("akka.tcp", "WorkerSys", install_ip, agent_port)
 
-    val ref = system.actorOf ((Apply2ActorProps (res.name,
+    val ref = system.actorOf ((getActorProps (res.name,
                                       res.installation,
                                       res.props,
                                       dep_map)).withDeploy (Deploy (scope = RemoteScope (remote_addr)))/*,
@@ -50,5 +55,8 @@ class MasterSystem (config: ResourceDesc) extends Bootable {
 
 object Main {
 
-  def main (args: Array[String]) { new MasterSystem (Apply2Install.plan) }
+  def main (args: Array[String]) { 
+    
+    new MasterSystem (Apply2Install.plan, Apply2ActorProps.apply)
+  }
 }
