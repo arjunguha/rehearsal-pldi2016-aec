642ace0c90683d33e177531fb92cfdedb4366f26
Nimish Gupta <nimish28@gmail.com>
2014-04-06 21:35:04 -0400

    Parser productions completed, regex parts of lexer remains

--- puppet/parser/src/main/scala/ast.scala
+++ puppet/parser/src/main/scala/ast.scala
@@ -16,9 +16,15 @@ sealed trait BoolBinOp
 case object And extends BoolBinOp
 case object Or  extends BoolBinOp
 
+
 sealed trait CompareOp
-case object Equal    extends CompareOp
-case object NotEqual extends CompareOp
+case object NotEqual    extends CompareOp
+case object Equal       extends CompareOp
+case object GreaterThan extends CompareOp
+case object GreaterEq   extends CompareOp
+case object LessThan    extends CompareOp
+case object LessEq  extends CompareOp
+
 
 sealed trait MatchOp
 case object Match   extends MatchOp
@@ -31,9 +37,11 @@ case object RightSimpleDep    extends RelationOp
 case object LeftSubscribeDep  extends RelationOp
 case object RightSubscribeDep extends RelationOp
 
+
 sealed trait AssignOp
 case object SimplAssign  extends AssignOp
 case object AppendAssign extends AssignOp
+
 
 sealed trait CollectionOp
 case object CollOr    extends CollectionOp
@@ -41,13 +49,17 @@ case object CollAnd   extends CollectionOp
 case object CollIsEq  extends CollectionOp
 case object CollNotEq extends CollectionOp
 
+
 sealed trait Functype
 case object Ftstmt extends Functype
 case object Ftrval  extends Functype
 
+
 sealed trait VirtualResType
 case object Vrtvirtual extends VirtualResType
 case object Vrtexported extends VirtualResType
+
+
 
 // AST
 
@@ -62,19 +74,18 @@ sealed abstract class Leaf extends AST
 
 // TODO : Refine, semantics are not clear yet
 case class ASTBool (val value: Boolean) extends Leaf
-case class ASTSTring (val value: String) extends Leaf
+case class ASTString (val value: String) extends Leaf
 case class FlatString (val value: String) extends Leaf // Uninterpreted String
-case class Concat extends Leaf
+case class Concat (val lhs: AST, val rhs: AST)  extends Leaf
 case class Default extends Leaf // Default class for case statement
-case class Type (val t: String) extends Leaf 
-case class Name (val name: String)  extends Leaf
+case class Type (val value: String) extends Leaf 
+case class Name (val value: String)  extends Leaf
 case class ClassName extends Leaf // Double colon separated class names
 case class Undef extends Leaf
-case class Hostname extends Leaf
-case class Variable extends Leaf
-case class HashOrArrayAccess extends Leaf
-case class Regex extends Leaf
-case object ASTNothing extends Leaf
+case class Hostname (val value: String) extends Leaf
+case class Variable (val value: String) extends Leaf
+case class HashOrArrayAccess (val variable: Leaf, var key: Branch) extends Leaf
+case class ASTRegex (val value: String) extends Leaf
 
 
 
@@ -85,11 +96,11 @@ case class BlockExpr (val exprs: List[Branch]) extends Branch
 case class ArithExpr    (val lval: Branch, val rval: Branch, val op: ArithOp)    extends Branch
 case class BoolBinExpr  (val lval: Branch, val rval: Branch, val op: BoolBinOp)  extends Branch
 case class CompareExpr  (val lval: Branch, val rval: Branch, val op: CompareOp)  extends Branch
-case class MatchExpr    (val lval: Branch, val rval: Branch, val op: MatchOp)    extends Branch
 case class RelationExpr (val lval: Branch, val rval: Branch, val op: RelationOp) extends Branch
+case class MatchExpr    (val lval: Branch, val rval: ASTRegex, val op: MatchOp)    extends Branch
 case class InExpr       (val lval: Branch, val rval: Branch) extends Branch
 case class NotExpr      (val oper: Branch) extends Branch
-case class Minus        (val oper: Branch) extends Branch
+case class UMinusExpr   (val oper: Branch) extends Branch
 
 
 // Variable definition
@@ -97,31 +108,34 @@ case class Vardef (val name: Leaf, val value: Branch, val op: AssignOp) extends 
 
 // Few Datastructures used by Puppet
 case class ASTArray (private val arr: List[Branch]) extends Branch
-case class ASTHash (val kvs: List[(Leaf, Branch)]) extends Leaf
+case class ASTHash (val kvs: List[(String, Branch)]) extends Leaf
 
 // Puppet Resource Decl Related nodes
 case class ResourceParam (val param: Leaf, val value: Branch, val add: Boolean) extends Branch
 case class ResourceInstance (val title: Leaf, val params: List[ResourceParam]) extends Branch
 case class Resource (val typ: ClassName, val instances: List[ResourceInstance]) extends Branch
-case class ResourceDefaults (val typ: Type, val params: List[ResourceParam]) extends Branch
-case class ResourceReference (val typ: Leaf, val title: List[Branch]) extends Branch
-case class ResourceOverride (val obj: ResourceReference, val params: List[ResourceParam]) extends Branch 
+case class ResourceDefaults (val typ: String, val params: List[ResourceParam]) extends Branch
+case class ResourceRef (val typ: Leaf, val title: List[Branch]) extends Branch
+case class ResourceOverride (val obj: ResourceRef, val params: List[ResourceParam]) extends Branch 
 case class VirtualResource (val res: Branch, val tvirt: VirtualResType) extends Branch
 
 // Conditional Statements
-case class If (val test: Branch, val true_exprs: BlockExpr, val false_exprs: BlockExpr) extends Branch
-case class CaseOpt (val value: List[Leaf], val exprs: BlockExpr) extends Branch
+case class IfExpr (val test: Branch, val true_exprs: BlockExpr, val false_exprs: BlockExpr) extends Branch
+case class CaseOpt (val value: List[AST], val exprs: BlockExpr) extends Branch
 case class CaseExpr (val test: Branch, val caseopts: List[CaseOpt]) extends Branch
-case class Selector (val param: Leaf, val values: List[ResourceParam]) extends Branch
+case class Selector (val param: AST, val values: List[ResourceParam]) extends Branch
 
 case class CollectionExpr (val lhs: Branch, val rhs: Branch, val op: CollectionOp) extends Branch
-case class CollectionExprTagNode (val coll: option[CollectionExpr], val prop: VirtualResType) extends Branch
+case class CollectionExprTagNode (val coll: Option[CollectionExpr], val prop: VirtualResType) extends Branch
 case class Collection (val typ: Type, val collectrhand: Branch, val params: List[ResourceParam]) extends Branch
 
-case class Node (val hostnames: List[Leaf], val parent: Leaf, exprs: BlockExpr) extends TopLevelConstruct
+case class Node (val hostnames: List[Hostname], val parent: Leaf, exprs: BlockExpr) extends TopLevelConstruct
 
-case class Hostclass (val classname: String, val args: List[Branch], val parent: Option[Leaf], stmts: List[Branch]) extends Branch
+case class Hostclass (val classname: String, val args: List[(String, Option[Branch])], val parent: Option[Leaf], stmts: BlockExpr) extends Branch
 
-case class Definition (val classname: String, val args: List[Branch], val exprs: BlockExpr) extends Branch
+case class Definition (val classname: String, val args: List[(String, Option[Branch])], val exprs: BlockExpr) extends TopLevelConstruct
 
 case class Function (val name: String, val args: List[AST], val ftype: Functype) extends Branch
+
+
+case class Import (val imports: List[String]) extends Branch
--- puppet/parser/src/main/scala/parser.scala
+++ puppet/parser/src/main/scala/parser.scala
@@ -11,8 +11,8 @@ class PuppetParse extends RegexParsers
   type P[+T] = PackratParser[T]
 
 
-  lazy val program: P[AST] =   stmts_and_decls 
-                             | (EofCh  ^^^ (ASTNothing))
+  lazy val program: P[BlockExpr] =   stmts_and_decls 
+                         /*    | (EofCh  ^^^ (BlockExpr (List[Branch] ()))) */
 
 
   lazy val stmts_and_decls: P[BlockExpr] = stmt_or_decl.* ^^ (BlockExpr (_))
@@ -36,13 +36,21 @@ class PuppetParse extends RegexParsers
 
 
   lazy val relationship_side: P[AST] = 
-    resource | resourceref | collection | variable | quotedtext | selector | casestatement |
+    resource | resourceref | collection | variable | quotedtext | selector | case_stmt |
     hasharrayaccesses
 
 
   lazy val fstmt: P[Function] = 
-    NAME ~ "(" ~> repsep(expr, ",") <~ ")" ^^ { case n ~ es => Function (n, es, Ftstmt) }
-  | NAME ~ repsep(rvalue, ",") ^^ { case n ~ rvs => Function (n, rvs, Ftstmt) }
+    NAME ~ "(" ~> expressions.? <~ ")" ^^ { 
+      case n ~ Some (es) => Function (n, es, Ftstmt)
+      case n ~ None => Function (n, List[AST] (), Fstmt)
+    }
+  | NAME ~ repsep(rvalue, ",") ^^ { 
+      case n ~ rvs => Function (n, rvs, Ftstmt)
+    }
+
+
+  lazy val expressions: P[AST] = repsep (expr, ("," | "=>"))
 
 
   lazy val rvalue: P[AST] =
@@ -72,17 +80,18 @@ class PuppetParse extends RegexParsers
 
   lazy val collection: P[Collection] =
     asttype ~ collectrhand ~ "{" ~> repsep (anyparam, ",") <~ "}" ^^ {
-      case t~cltrhnd~anyparams => Collection (t, cltrhnd, anyparams)
+      case t ~ cltrhnd ~ anyparams => Collection (t, cltrhnd, anyparams)
     }
-  | asttype ~ collectrhand ^^ { case t~cltrhnd => Collection (t, cltrhnd, []) }
+  | asttype ~ collectrhand ^^ { 
+      case t ~ cltrhnd => Collection (t, cltrhnd, List[ResourceParam] ())
+    }
 
 
   lazy val collectrhand: P[CollectionExprTagNode] =
-    "<|" ~> collstmts <~ "|>" ^^ (CollectionExprTagNode (_, Vrtvirtual))
-  | "<<|" ~> collstmts <~ "|>>" ^^ (CollectionExprTagNode (_, Vrtexported))
+    "<|" ~> collstmts.? <~ "|>" ^^ (CollectionExprTagNode (_, Vrtvirtual))
+  | "<<|" ~> collstmts.? <~ "|>>" ^^ (CollectionExprTagNode (_, Vrtexported))
 
   lazy val collstmts: P[Option[CollectionExpr]] =
-    "" ^^ (None)
   | collstmt ^^ (Some _)
   | collstmts ~ ("and" | "or") ~ collstmt ^^ {
       case x ~ "and" ~ y => Some (CollectionExpr (x, y, CollAnd)
@@ -147,17 +156,245 @@ class PuppetParse extends RegexParsers
   lazy val keyword: P[LEAF] = /* TODO */
 
 
-  lazy val param: P
+  lazy val param: P[ResourceParam] = 
+    param_name <~ "=>" ~> expr ^^ {
+      case pn ~ e => ResourceParam (pn, e, false)
+    }
 
 
+  lazy val addparam: P[ResourceParam] = 
+    NAME <~ "+>" ~> expr ^^ {
+      case name ~ e => ResourceParam (name, e, true)
+    }
 
 
+  lazy val anyparams: P[ResourceParam] = repsep ((param | addparam), ",")
 
 
+  lazy funcrvalue: P[Function] = 
+    NAME <~ "(" ~> expressions.? <~ ")" ^^ {
+      case name ~ Some (es) => Function (name, es,           Ftrval)
+      case name ~ None      => Function (name, List[AST] (), Ftrval)
+    }
 
 
+  lazy val quotedstring: P[Leaf] = 
+    STRING ^^ (ASTString (_))
+  | (DQPRE ~ dqrval ^^ {
+      case x ~ y => Concat (x, y)
+    })
+
+
+  lazy val dqrval: P[List[AST]] =
+    expr ~ dqtail ^^ {
+      case e ~ y => e :: y
+    }
+
+
+  lazy val dqtail: P[List[AST]] =
+    DQPOST ^^ (List[ASTString] (ASTString (_)))
+  | DQMID ~ dqrval ^^ {
+      case x ~ y => x :: y
+    }
+
+
+  lazy val boolean: P[ASTBool] =
+    BOOLEAN ^^ {
+      case "true" => ASTBool (true)
+      case "false" => ASTBool (false)
+    }
+
+
+  lazy val resourceref: P[ResourceRef] =
+    NAME <~ "[" ~> expressions <~ "]" ^^ {
+      case name ~ e => ResourceRef (name, e)
+    }
+  | asttype <~ "[" ~> expressoins <~ "]" ^^ {
+      case t ~ e => ResourceRef (t.value, e)
+    }
+
+
+  // TODO: stmts_and_decls can return "nothing", do we really need to "repeat" ourself
+  //      with ".?"
+  lazy val unless_stmt: P[NotExpr] = 
+    "unless" ~> expr <~ "{" ~> stmts_and_decls.? ~> "}" ^^ {
+      case e ~ Some (ss) => IfExpr (NotExpr (e), ss,                  BlockExpr (List ()))
+      case e ~ None      => IfExpr (NotExpr (e), BlockExpr (List ()), BlockExpr (List ()))
+    }
+
+
+  lazy val ifstmt_begin: P[IfExpr] = "if" ~> ifstmt
+
+
+  lazy val ifstmt: P[IfExpr] = 
+    expr <~ "{" ~> stmts_and_decls.? <~ "}" ~> elsestmt.? ^^ {
+      case e ~ None      ~ None      => IfExpr (e, BlockExpr (List ()), BlockExpr (List ()))
+      case e ~ Some (ss) ~ None      => IfExpr (e, ss,                  BlockExpr (List ()))
+      case e ~ None      ~ Some (es) => IfExpr (e, BlockExpr (List ()), es)
+      case e ~ Some (ss) ~ Some (es) => IfExpr (e, ss, es)
+    }
+
+
+  // TODO: stmts_and_decls can return "nothing", do we really need to "repeat" ourself
+  //      with ".?"
+  lazy val elsestmt: P[BlockExpr] = 
+    ("elsif" ~> ifstmt ^^ (BlockExpr (List (_))))
+  | "else" ~> "{" ~> stmts_and_decls.? <~ "}" ^^ {
+      case None => BlockExpr (List ())
+      case Some ss => BlockExpr (ss)
+    }
+
+
+  lazy val expr: P[Branch] = 
+    rvalue | hash
+  | ("-" ~ expr ^^ (UMinusExpr (_)))
+  | ("!" ~ expr ^^ (NotExpr (_)))
+  | "(" ~> expr <~ ")"
+  | (expr ~ ("=~" | "!~") ~ regex ^^ {
+      case e ~ "=~" r => MatchExpr (e, r, Match)
+      case e ~ "!~" r => MatchExpr (e, r, NoMatch)
+    })
+  | (expr ~ ("in" | "+" | "-" | "/" | "*" | "%" | "<<" | ">>" | "!=" | "==" | ">" | ">=" | "<" | "<=" | "and" | "or") ~ expr ^^ {
+      case e1 ~ "in" ~ e2 => InExpr (e1, e2)
+      case e1 ~ "+"  ~ e2 => ArithExpr (e1, e2, Plus)
+      case e1 ~ "-"  ~ e2 => ArithExpr (e1, e2, Minus)
+      case e1 ~ "/"  ~ e2 => ArithExpr (e1, e2, Div)
+      case e1 ~ "*"  ~ e2 => ArithExpr (e1, e2, Mult)
+      case e1 ~ "%"  ~ e2 => ArithExpr (e1, e2, Mod)
+      case e1 ~ "<<" ~ e2 => ArithExpr (e1, e2, LShift)
+      case e1 ~ ">>" ~ e2 => ArithExpr (e1, e2, RShift)
+      case e1 ~ "!=" ~ e2 => CompareExpr (e1, e2, NotEqual)
+      case e1 ~ "==" ~ e2 => CompareExpr (e1, e2, Equal)
+      case e1 ~ ">"  ~ e2 => CompareExpr (e1, e2, GreaterThan) 
+      case e1 ~ ">=" ~ e2 => CompareExpr (e1, e2, GreaterEq)
+      case e1 ~ "<"  ~ e2 => CompareExpr (e1, e2, LessThan)
+      case e1 ~ "<=" ~ e2 => CompareExpr (e1, e2, LessEq)
+      case e1 ~ "and" ~ e2 => BoolBinOp (e1, e2, And)
+      case e1 ~ "or" ~ e2 =>  BoolBinOp (e1, e2, Or)
+    })
+
+  lazy val case_stmt: P[CaseExpr] = 
+    "case" ~> expr <~ "{" ~> caseopts <~ "}" ^^ {
+      case e ~ csopts => CaseExpr (e, csopts)
+    }
+
+  lazy val caseopts: P[List[CaseOpt]] = 
+    (caseopt ^^ (List (_))) 
+  | (caseopts ~ caseopt ^^ {
+      case cs ~ c => cs :+ c
+  }
+
+  // TODO: stmts_and_decls can return "nothing", do we really need to "repeat" ourself
+  //      with ".?"
+  lazy val caseopt: P[CaseOpt] =
+    casevalues <~ ":" ~> "{" ~> stmts_and_decls.? <~ "}" ^^ {
+      case csvs ~ None => CaseOpt (csvs, BlockExpr (List (_)))
+      case csvs ~ Some (ss) => CaseOpts (csvs, ss)
+    }
+
+  lazy val casevalues: P[List[AST]] = repsep (selectlhand, ",")
+
+
+  lazy val selector: P[Selector] =
+    selectlhand <~ "?" ~> svalues ^^ {
+      case slctlhnd ~ svals => Selector (slctlhnd, svals)
+    }
+
+
+  lazy val svalues: P[AST] = selectval | "{" ~> sintvalues <~ ",".? <~ "}"
+
+
+  lazy val sintvalues: P[ASTArray] = repsep (selectval, ("," | "=>")) ^^ (ASTArray (_))
+
+
+  lazy val selectval: P[ResourceParam] = 
+    selectlhand <~ "=>" ~> rvalue ^^ {
+      case slcthnd ~ rval => ResourceParam (slcthnd, rval, false)
+    }
+
+
+  lazy val selectlhand: P[AST] = 
+    name | asttype | quotedtext | variable | funcrvalue | boolean | undef |
+    hasharrayaccess | ("default" ^^^ Default) | regex
     
+  
+  lazy val string: P[String] = STRING
 
+  lazy val strings: P[String] = 
+    repsep (string, ",") ^^ (_.FoldLeft ("") { case acc, el => acc + el }
 
-  // TODO : Name has to be regex
+  lazy val import: P[Import] = 
+    "import" ~> string ^^ (Import (_))
+
+  lazy val definition: P[Definition] = 
+    "define" ~> classname ~ argumentlist <~ "{" ~> stmts_and_decls.? <~ "}" ^^ {
+      case cnm ~ args ~ None      => Definition (cnm, args, BlockExpr (List ()))
+      case cnm ~ args ~ Some (ss) => Definition (cnm, args, ss)
+    }
+
+  lazy val hostclass: P[Hostclass] = 
+    "class" ~> classname ~ argumentlist ~ classparent.? <~ "{" ~> stmts_and_decls.? <~ "}" ^^ {
+      case cnm ~ args ~ clp ~ None => Hostclass (cnm, args, clp, BlockExpr (List ()))
+      case cnm ~ args ~ clp ~ Some (ss) => Hostclass (cnm, args, clp, ss)
+    }
+
+  lazy val nodedef: P[Node] =
+    "node" ~> hostnames ~ nodeparent.? <~ "{" stmts_and_decls.? <~ "}" ^^ {
+      case hosts ~ ndp ~ None => Node (hosts, ndp, BlockExpr (List ()))
+      case hosts ~ ndp ~ Some (ss) => Node (hosts, ndp, ss)
+    }
+
+  lazy val classname: P[String] = NAME | "class"
+
+  lazy val hostnames: P[List[Hostname] = repsep (nodename, ",") 
+
+  lazy val nodename: P[Hostname] = hostname ^^ (Hostname (_))
+
+  lazy val hostname: P[String] = NAME | STRING | "default" | regex
+
+  lazy val argumentlist: P[List[(String, Option[Branch])]] = 
+    "(" ~> arguments.? <~ ")" | "(" ~> arguments <~ ",".? <~ ")"
+
+  lazy val arguments: P[List[(String, Option[Branch])]] = repsep (argument, ",")
+
+  lazy val argument: P[(String, Option[Branch])] = 
+    VARIABLE <~ "=" ~> expr ^^ { case v ~ e => (v, Some (e)) }
+  | VARIABLE ^^ ((_, None))
+
+  lazy val nodeparent: P[String] = "inherits" ~> hostname
+
+  lazy val classparent: P[String] = "inherits" ~> (classname | "default")
+
+  lazy val variable: P[Variable] = VARIABLE ^^ (Variable (_))
+
+  lazy val array: P[ASTArray] =
+    "[" ~> expressions.? "]" | "[" ~> expression <~ "," <~ "]" ^^ (ASTArray (_))
+
+  lazy val regex: P[ASTRegex] = REGEX ^^ (ASTRegex (_))
+
+  lazy val hash: P[ASTHash] = 
+    "{" ~> hashpairs.? <~ "}" | "{" ~> hashpairs <~ ",".? <~ "}" ^^ (ASTHash (_))
+
+  lazy val hashpairs: P[List[(String, Branch)]] = repsep (hashpair, ",")
+
+  lazy val hashpair: P[(String, Branch)] = key <~ "=>" ~> expr ^^ {
+    case k ~ e => (k, e)
+  }
+
+  lazy val key: P[String] = NAME | quotedtext
+
+  lazy val hasharrayaccess: P[HashOrArrayAccess] =
+    variable <~ "[" ~> expr <~ "]" ^^ {
+      case v ~ e => HashOrArrayAccess (v, e)
+    }
+
+  lazy val hasharrayaccesses: P[HashOrArrayAccess] = hasharrayaccess 
+  | hasharrayaccesses <~ "[" ~> expr <~ "]" ^^ {
+      case haa ~ e => HashOrArrayAccess (haa, e)
+    }
+    
+  // TODO : NAME
+  // TODO : STRING
+  // TODO : REGEX
+  // TODO : quotedtext
 }
