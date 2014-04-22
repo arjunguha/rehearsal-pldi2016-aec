8f4b9354c8698efa74a7563608ac152c660b4668
Nimish Gupta <nimish28@gmail.com>
2014-04-22 02:39:46 -0400

    cleanup

--- puppet/semantics/manifests/package/unstable_orig.pp
+++ puppet/semantics/manifests/package/unstable_orig.pp
@@ -1,39 +1,8 @@
-/*
-node 'agent1' {
-
-  file { 'a':
-
-    path    => '/home/vagrant/dir1',
-    ensure  => absent,
-    # force   => true
-  }
-
-
-  file { 'b':
-
-    path    => '/home/vagrant/dir1/file1',
-    ensure  => present,
-    content => 'Test file',
-    require => File['testdir']
-  }
+package { 'python-oops':
+  ensure => absent
 }
-*/
 
-
-/*
- * 2 packages dependendency a -> b 
- * delete 'a' from its spec
- * without require
- */
-
-node 'agent1' {
-
-  package { 'python-oops':
-    ensure => absent
-  }
-
-  package { 'python-oops-wsgi':
-    ensure => installed,
-    require => Package['python-oops']
-  }
+package { 'python-oops-wsgi':
+  ensure  => installed,
+  require => Package['python-oops']
 }
