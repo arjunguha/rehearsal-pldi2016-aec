5539ce69eb6998f7cf6fad2c22a52dac15b715de
Nimish Gupta <nimish28@gmail.com>
2014-08-04 10:07:40 -0400

    compilatio of few Bechmarks covered by unit tests

---
+++ puppet/parser/src/test/scala/BenchmarkSuite.scala
@@ -0,0 +1,33 @@
+import puppet.driver._
+import org.scalatest.FunSuite
+
+
+class BenchmarkSuite extends FunSuite {
+
+  case class Benchmark(mainFile: String, modulePath: Option[String] = None)
+
+  val benchmarkroot = "../benchmarks"
+
+  val benchmarks = Map("puppet-bind" -> Benchmark("src/tests/server.pp", Some("src/modules")),
+                       "puppet-git" -> Benchmark("src/tests/init.pp", Some("src/modules")),
+                       "puppet-mosh" -> Benchmark("src/tests/init.pp", Some("src/modules")),
+                       "vagrant-cakephp" -> Benchmark("src/manifests/site.pp",
+                                                    Some("src/modules")),
+                       "vagrantpress" -> Benchmark("src/manifests/site.pp",
+                                                 Some("src/modules"))
+                       // TODO : Add more tests
+                    /*"puppet-rbenv" -> Benchmark("src/tests/init.pp", Some("src/modules"))*/
+                       )
+
+  for ((name, b) <- benchmarks) {
+    import puppet.driver.{PuppetDriver => driver}
+
+    test (s"compiling benchmark: $name") {
+      val mainFilePath = s"${benchmarkroot}/${name}/${b.mainFile}"
+      val modulePath = b.modulePath.map((p) => s"${benchmarkroot}/${name}/${p}")
+      val content = driver.prepareContent(mainFilePath, modulePath)
+      val graph = driver.compile(content)
+      // driver.verify(graph)
+    }
+  }
+}
