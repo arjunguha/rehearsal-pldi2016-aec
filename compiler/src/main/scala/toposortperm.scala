baf6753398655649c9ca6d61925cf00869b9a9b5
Nimish Gupta <nimish@cs.umass.edu>
2014-08-13 14:51:13 -0400

    lazy recursion tree generation for all permutations of resource graph(dag)

--- compiler/src/main/scala/toposortperm.scala
+++ compiler/src/main/scala/toposortperm.scala
@@ -4,14 +4,34 @@ import scala.collection._
 import scalax.collection.Graph
 import scalax.collection.GraphEdge.DiEdge
 
+import scala.collection.immutable.Stream
+
+class Tree[T](val root: T, val children: Stream[Tree[T]])
+
+/* Lazy generation of recursion tree for all permutations of graph */
+object TopoSortPermutationTree {
+
+  private def subtrees[T](options: List[Graph[T, DiEdge]#NodeT])
+                         (implicit dag: Graph[T, DiEdge]): Stream[Tree[T]] = options match {
+    case n :: ns => new Tree(n.value, TopoSortPermutationTree(dag - n)) #:: subtrees(ns)
+    case Nil => Stream.empty
+  }
+
+  def apply[T](dag: Graph[T, DiEdge]): Stream[Tree[T]] =
+    subtrees(dag.nodes.filter(_.inDegree == 0).toList)(dag)
+}
+
 object GraphTopoSortPermutations {
 
-  // TODO : Not Efficient, Not Tail recursive
-  private def permutations[N] (ord: Seq[N], subg: Graph[N,DiEdge]): Set[Seq[N]] =
+  private def generatePermutations[N] (ord: Seq[N], subg: Graph[N,DiEdge]): Set[Seq[N]] = 
     if(subg.isEmpty) Set(ord)
-    else subg.nodes.filter(_.inDegree == 0).map((n) => permutations(ord :+ n.value, subg - n)).flatten
+    else subg.nodes.filter(_.inDegree == 0).map((n) => generatePermutations(ord :+ n.value, subg - n)).flatten
 
-  // TODO : Handle cycles    
   // Returns a sequence of all topological sorts of a graph
-  def apply[N](g: Graph[N, DiEdge]): Set[Seq[N]] = permutations(Seq[N](), g)
+  def apply[N](g: Graph[N, DiEdge]): Set[Seq[N]] = {
+    if(g.isCyclic)
+      throw new Exception("Topological sort not possible for cyclic graph")
+      
+    generatePermutations(Seq[N](), g)
+  }
 }
