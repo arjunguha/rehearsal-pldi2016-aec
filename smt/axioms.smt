92075a7417c057db0d00240d577f93399625363f
Nimish Gupta <nimish@cs.umass.edu>
2014-10-02 02:07:29 -0400

    mkdir commutes in scala-z3

--- smt/axioms.smt
+++ smt/axioms.smt
@@ -2,23 +2,23 @@
 
 ;(declare-datatypes () ((Path root a b c d)))
 
-(declare-const root Path)
-(declare-const a Path)
-(declare-const b Path)
-(declare-const c Path)
-(declare-const d Path)
+;(declare-const root Path)
+;(declare-const a Path)
+;(declare-const b Path)
+;(declare-const c Path)
+;(declare-const d Path)
 
-(define-fun is-ancestor ((p1 Path) (p2 Path)) Bool
-  (or (= p1 p2)
-      (= root p2)
-      (= a d)))
+;(define-fun is-ancestor ((p1 Path) (p2 Path)) Bool
+;  (or (= p1 p2)
+;      (= root p2)
+;      (= a d)))
 
 
 ; S is a file-system operation, or error
-(declare-sort S)
+; (declare-sort S)
 
-(declare-fun err () S)
-(declare-fun seq (S S) S)
+;(declare-fun err () S)
+;(declare-fun seq (S S) S)
 
 ; err is the zero
 (assert (forall ((op S)) (= (seq op err) err)))
@@ -27,7 +27,7 @@
 ; seq is associative
 (assert (forall ((a S) (b S) (c S)) (= (seq a (seq b c)) (seq (seq a b) c))))
 
-(declare-fun mkdir (Path) S)
+;(declare-fun mkdir (Path) S)
 
 ; mkdir is commutative
 (assert
@@ -35,7 +35,7 @@
    (=> (and (not (is-ancestor p1 p2)) (not (is-ancestor p2 p1)))
        (= (seq (mkdir p1) (mkdir p2)) (seq (mkdir p2) (mkdir p1))))))
 
-(declare-fun create (Path) S)
+;(declare-fun create (Path) S)
 
 ; create is commutative
 (assert
--- src/equiv/src/test/scala/BasicEquivTestSuite.scala
+++ src/equiv/src/test/scala/BasicEquivTestSuite.scala
@@ -5,36 +5,80 @@ class Core extends FunSuite with Matchers {
   import z3.scala._
   import z3.scala.dsl._
 
+  def mkZ3Sort(name: String)
+              (implicit z3: Z3Context): (Z3Symbol, Z3Sort) = {
+    val symbol = z3.mkStringSymbol(name)
+    (symbol, z3.mkUninterpretedSort(symbol))
+  }
+
+  def mkZ3Func(name: String, DomainSorts: Seq[Z3Sort], RangeSort: Z3Sort)
+              (implicit z3: Z3Context): (Z3Symbol, Z3FuncDecl) = {
+    val symbol = z3.mkStringSymbol(name)
+    (symbol, z3.mkFuncDecl(symbol, DomainSorts, RangeSort))
+  }
+              
+
   test("Core") {
-    val z3 = new Z3Context(new Z3Config("MODEL" -> true))
+    implicit val z3 = new Z3Context(new Z3Config("MODEL" -> true))
 
-    val pathSymbol = z3.mkStringSymbol("Path")
-    val Z3Path = z3.mkUninterpretedSort(pathSymbol)
-    // val x = z3.mkStringSymbol("x")
-    // val x = z3.mkFreshConst("x", z3.mkBoolSort)
-    // val y = z3.mkFreshConst("y", z3.mkBoolSort)
+    val (pathSymbol, pathSort) = mkZ3Sort("Path")
+    val (sSymbol, sSort) = mkZ3Sort("S")
+ 
+    val (errSymbol, err) = mkZ3Func("err", Seq(), sSort)
+    val (seqSymbol, seq) = mkZ3Func("seq", Seq(sSort, sSort), sSort)
+    val (ancSymbol, is_ancestor) = mkZ3Func("is-ancestor", Seq(pathSort, pathSort), z3.mkBoolSort)
+    val (mkdirSymbol, mkdir) = mkZ3Func("mkdir", Seq(pathSort), sSort);
+    val (createSymbol, create) = mkZ3Func("create", Seq(pathSort), sSort)
 
+    val sortMap = Map(pathSymbol -> pathSort, sSymbol -> sSort);
+    val funcMap = Map(errSymbol -> err, seqSymbol -> seq,
+                       ancSymbol -> is_ancestor, mkdirSymbol -> mkdir,
+                       createSymbol -> create)
 
     assert(java.nio.file.Files.isRegularFile(java.nio.file.Paths.get("../smt/axioms.smt")))
-    val ast = z3.parseSMTLIB2File("../smt/axioms.smt", Map(pathSymbol -> Z3Path), Map.empty)
+    z3.parseSMTLIB2File("../smt/axioms.smt", sortMap, funcMap)
 
-//    println (z3.getASTKind(ast));
+    // declare-consts
+    val rootPathSymbol = z3.mkStringSymbol("root")
+    val aPathSymbol = z3.mkStringSymbol("a")
+    val bPathSymbol = z3.mkStringSymbol("b")
+    val cPathSymbol = z3.mkStringSymbol("c")
+    val dPathSymbol = z3.mkStringSymbol("d")
 
-//    println(z3.astToString(ast));
+    val root = z3.mkConst(rootPathSymbol, pathSort)
+    val a = z3.mkConst(aPathSymbol, pathSort)
+    val b = z3.mkConst(bPathSymbol, pathSort)
+    val c = z3.mkConst(cPathSymbol, pathSort)
+    val d = z3.mkConst(dPathSymbol, pathSort)
 
-    // val x = 
-    // val y = 
 
-//    println("working")
+    /* forall ((p1 Path) (p2 Path))
+     *   (= (is-ancestor p1 p2)
+     *       (or (= p1 p2)
+     *          (= root p2)
+     *          (= a d)))
+     */
+    val p1 = z3.mkBound(0, pathSort)
+    val p2 = z3.mkBound(1, pathSort)
+    val pattern = z3.mkPattern(is_ancestor(p1 , p2)) // Not sure
+    val axiomtree = z3.mkImplies(is_ancestor(p1, p2), z3.mkOr(p1 === p2, root === p2, a === d))
 
+    val p1Symbol: Z3Symbol = z3.mkStringSymbol("p1")
+    val p2Symbol: Z3Symbol = z3.mkStringSymbol("p2")
+    val is_ancestor_defn = z3.mkForAll(0, List(pattern), List((p1Symbol, pathSort), (p2Symbol, pathSort)), axiomtree)
 
+    // Commutative
+    val commute_axiom = ((seq(mkdir(a), mkdir(b))) !== (seq(mkdir(a),(mkdir(b)))))
 
     val solver = z3.mkSolver
     // val expr = (x !== y)
+
     //solver.assertCnstr(expr.ast(z3))
-    // solver.assertCnstr(expr)
+
+    solver.assertCnstr(is_ancestor_defn)
+    solver.assertCnstr(commute_axiom)
     println(solver.checkAssumptions())
-    println(solver.getModel())
+    // println(solver.getModel())
     // solver.assertCnstr(p2 --> !(y === zero))
     // solver.assertCnstr(p3 --> !(x === zero))
 
