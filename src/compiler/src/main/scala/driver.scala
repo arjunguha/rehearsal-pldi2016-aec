f0122a6f25dffdf766bc5294d27fcea03c5c7463
Nimish Gupta <nimish@cs.umass.edu>
2014-11-11 13:55:45 -0500

    refactoring

--- src/compiler/src/main/scala/driver.scala
+++ src/compiler/src/main/scala/driver.scala
@@ -67,48 +67,33 @@ object PuppetDriver {
     g
   }
 
-  /*
-  def verify(g: Graph[Resource, DiEdge]): Boolean = {
+  def printDOTGraph(g: Graph[Resource, DiEdge]) {
+    import scalax.collection.io.dot._
+    import scala.language.existentials
 
-    import puppet.runtime.toposortperm._
-    import puppet.verification._
-    import scala.concurrent._
-    import scala.concurrent.duration._
-    import scala.concurrent.Future
-    import scala.concurrent.ExecutionContext.Implicits.global
+    val root = DotRootGraph(
+      directed = true,
+      id = Some("Resource_Graph"),
+      kvList = Seq[DotAttr]())
 
-    import scala.util.{Try, Success, Failure}
-
-    val permutationTrees = TopoSortPermutationTree(g)
-    val resFut = Verify(permutationTrees.toSeq:_*)
-    */
-
-    /*
-    val lstOfFuts = permutationTrees.toList.map((t) => Verify(t))
-    val lstOfFutsTry = lstOfFuts.map(f => f.map(Success(_)).recover { case e => Failure(e) })
-    val futOfLst = Future.sequence(lstOfFutsTry)
-    val finalVal = Promise[Boolean]()
-    futOfLst onSuccess { case lstTryB => finalVal.success(lstTryB.map(_ getOrElse false).foldLeft(true)(_ && _)) }
-    futOfLst onFailure { case e => finalVal.success(false) }
-    val res = Await.result(finalVal.future, Duration.Inf)
-    // PuppetActorSystem.system.shutdown()
-    res
-    */
-    /*
-    Await.result(resFut, Duration.Inf)
-  }
-  */
-
-  /*
-  def verifyLocal(g: Graph[Resource, DiEdge]) {
-    println("Number of resources in graph: " + g.nodes.size)
-    val permutations = GraphTopoSortPermutations(g)
-    println("Number of permutations: " + permutations.size)
-    for (p <- permutations) {
-      processInstallOrderLocal(p)
+    def edgeTransformer(innerEdge: Graph[Resource, DiEdge]#EdgeT):
+      Option[(DotGraph, DotEdgeStmt)] = {
+      val edge = innerEdge.edge
+      Some(root, DotEdgeStmt(edge.from.toString,
+                             edge.to.toString,
+                             Nil))
     }
+
+    def iNodeTransformer(isolatedNode: Graph[Resource, DiEdge]#NodeT):
+      Option[(DotGraph, DotNodeStmt)] = {
+      Some(root, DotNodeStmt(isolatedNode.toString, Nil))
+    }
+
+    val dot = g.toDot(root, edgeTransformer,
+      None, None, Some(iNodeTransformer))
+
+    println(dot)
   }
-  */
 
 
   def toCoreValue(v: Value): resrc.Value = v match {
@@ -161,44 +146,22 @@ object PuppetDriver {
     lazy val system = ActorSystem("PuppetInstallerSystem", akkaConf)
   }
 
-  def verify(g: Graph[Resource, DiEdge]): Boolean = {
+  def toSerializable(g: Graph[Resource, DiEdge]): Graph[resrc.Resource, DiEdge] = {
     // Convert to serializable Graph
     val nodes = g.nodes map ((n) => toCoreResource(n.value))
     val edges = g.edges map ((e) => toCoreResource(e.source.value) ~> toCoreResource(e.target.value))
-    val gprime = Graph.from(nodes, edges)
+    Graph.from(nodes, edges)
+  }
+
+  def verify(g: Graph[Resource, DiEdge]): Boolean = {
 
     import puppet.common._
+
+    val gprime = toSerializable(g)
+
 
     val master = PuppetActorSystem.system.actorSelection(PuppetActorSystem.masterAddress)
     master ! Work(gprime)
     true
-  }
-
-  def printDOTGraph(g: Graph[Resource, DiEdge]) {
-    import scalax.collection.io.dot._
-    import scala.language.existentials
-
-    val root = DotRootGraph(
-      directed = true,
-      id = Some("Resource_Graph"),
-      kvList = Seq[DotAttr]())
-
-    def edgeTransformer(innerEdge: Graph[Resource, DiEdge]#EdgeT):
-      Option[(DotGraph, DotEdgeStmt)] = {
-      val edge = innerEdge.edge
-      Some(root, DotEdgeStmt(edge.from.toString,
-                             edge.to.toString,
-                             Nil))
-    }
-
-    def iNodeTransformer(isolatedNode: Graph[Resource, DiEdge]#NodeT):
-      Option[(DotGraph, DotNodeStmt)] = {
-      Some(root, DotNodeStmt(isolatedNode.toString, Nil))
-    }
-
-    val dot = g.toDot(root, edgeTransformer,
-      None, None, Some(iNodeTransformer))
-
-    println(dot)
   }
 }
