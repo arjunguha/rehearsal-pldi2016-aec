234dbd365597ba2201892b9f192299855076c4e7
KaiGeffen <geffenkai@gmail.com>
2014-12-20 14:26:19 -0500

    Established type of State used by Eval, gitignore .DS_Store

--- src/main/scala/Eval.scala
+++ src/main/scala/Eval.scala
@@ -1,10 +1,13 @@
 package fsmodel
 
+import java.nio.file.Path
+
 object Eval {
 
-  /*
-  type State = ... ???
 
+  type State = Map[Path, FileState]
+
+  /*
   // Perhaps Set[State] (may be unnecessarily complex)
   def eval(p: Expr, s: State): List[State] = ...
 
