737c04b69eb6e98769964b5a50abe291b0bd8e9d
KaiGeffen <geffenkai@gmail.com>
2014-12-20 15:27:38 -0500

    Beginning to write tests

--- src/main/scala/Eval.scala
+++ src/main/scala/Eval.scala
@@ -7,11 +7,25 @@ object Eval {
 
   type State = Map[Path, FileState]
 
-  /*
+  
   // Perhaps Set[State] (may be unnecessarily complex)
-  def eval(p: Expr, s: State): List[State] = ...
+  def eval(p: Expr, s: State): List[State] = p match {
+		case Error => List(s)
+		case Skip => List(s)
+		case _ => List(s)
+		/*
+		case Block(p, q) =>
+		case Alt(p, q) =>
+		case If(pred, p, q) =>
+		case Mkdir(path) =>
+		case CreateFile(path, hash) =>
+		case Rm(path) =>
+		case Cp(src, dst) =>
+		case Mv(src, dst) =>
+		*/
+  }
 
-  */
+  
 
 
 }
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -1,8 +1,35 @@
 package fsmodel
 
+import java.nio.file.Path
+
 import Implicits._
 
 class TestSuite extends org.scalatest.FunSuite {
+
+  val emptyState: Map[Path, FileState] = Map()
+  val existingDir = "/pictures"
+  // TODO(kgeffen) Improve this line
+  val startState: Map[Path, FileState] = Map(stringToPath("/usr") -> IsDir,
+                                             stringToPath("/usr/kai") -> IsDir,
+                                             stringToPath(existingDir) -> IsDir,
+                                             stringToPath("/usr/kai/config") -> IsFile)
+
+  // TODO(kgeffen) Better title
+  test("Error returns empty list of possible states") {
+    assert(Eval.eval(Error, startState) == List())
+  }
+
+  test("Skip does not change state") {
+    assert(Eval.eval(Skip, startState) == List(startState))
+  }
+
+  test("Mkdir creates file if parent exists and dir does not yet exist") {
+    val newDir = existingDir + "/cats"
+
+    assertResult(Eval.eval(Mkdir(newDir), startState)) {
+      startState ++ Map(newDir -> IsDir)
+    }
+  }
 
   test("can constuct expressions") {
 
