fd82509c90d68e59045f5357ac13d2f815feb310
KaiGeffen <geffenkai@gmail.com>
2014-12-21 20:38:45 -0500

    Implemented Mv, Cp, Block; All tests pass

--- src/main/scala/Eval.scala
+++ src/main/scala/Eval.scala
@@ -17,20 +17,35 @@ object Eval {
 			case _ if !s.contains(path.getParent) => List()
 			case _ => List(s + (path -> IsDir))
 		}
+		// NOTE(kgeffen) File contents not yet used
 		case CreateFile(path, hash) => path match {
 			case _ if s.contains(path) => List()
 			case _ if !s.contains(path.getParent) => List()
 			case _ => List(s + (path -> IsFile))
 		}
+		case Cp(src, dst) => dst match {
+			case _ if s.contains(dst) => List()
+			case _ if !s.contains(dst.getParent) => List()
+			case _ => s.get(src) match {
+				case None => List()
+				case Some(srcState) => srcState match {
+					case DoesNotExist => List()
+					case IsDir => eval(Mkdir(dst), s)
+					// TODO(kgeffen) When contents are used, include contents here
+					case IsFile => eval(CreateFile(dst), s)
+				}
+			}
+		}
+		case Mv(src, dst) => eval(Block(Cp(src, dst), Rm(src)), s)
 		case Rm(path) => path match {
 			case _ if !s.contains(path) => List()
 			// If path is an occupied dir (Has any children)
 			case _ if s.keys.exists(k => k.getParent == path) => List()
 			case _ => List(s - path)
 		}
-		// case Block(p1, p2) => {
-		// 	eval(p1, s).flatMap(newState => eval(p2, newState))
-		// }
+		case Block(p1, p2) => {
+			eval(p1, s).flatMap(newState => eval(p2, newState))
+		}
 		case _ => List(s)
 		/*
 		case Block(p, q) =>
