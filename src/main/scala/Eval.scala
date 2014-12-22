4a0922f54f338436f44883da2ad1cc1d976b4689
KaiGeffen <geffenkai@gmail.com>
2014-12-21 20:16:34 -0500

    Roughly implemented Mkdir, CreateFile, Rm

--- src/main/scala/Eval.scala
+++ src/main/scala/Eval.scala
@@ -9,9 +9,28 @@ object Eval {
 
   
   // Perhaps Set[State] (may be unnecessarily complex)
-  def eval(p: Expr, s: State): List[State] = p match {
-		case Error => List(s)
+  def eval(pred: Expr, s: State): List[State] = pred match {
+		case Error => List()
 		case Skip => List(s)
+		case Mkdir(path) => path match {
+			case _ if s.contains(path) => List()
+			case _ if !s.contains(path.getParent) => List()
+			case _ => List(s + (path -> IsDir))
+		}
+		case CreateFile(path, hash) => path match {
+			case _ if s.contains(path) => List()
+			case _ if !s.contains(path.getParent) => List()
+			case _ => List(s + (path -> IsFile))
+		}
+		case Rm(path) => path match {
+			case _ if !s.contains(path) => List()
+			// If path is an occupied dir (Has any children)
+			case _ if s.keys.exists(k => k.getParent == path) => List()
+			case _ => List(s - path)
+		}
+		// case Block(p1, p2) => {
+		// 	eval(p1, s).flatMap(newState => eval(p2, newState))
+		// }
 		case _ => List(s)
 		/*
 		case Block(p, q) =>
