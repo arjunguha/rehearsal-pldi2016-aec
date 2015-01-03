f6675b3da7d0f1b157ac4f17f11ab042090d3af0
KaiGeffen <geffenkai@gmail.com>
2015-01-03 14:16:37 -0500

    Began writing tests for typedz3

--- src/test/scala/TypedZ3Tests.scala
+++ src/test/scala/TypedZ3Tests.scala
@@ -1,11 +1,19 @@
 package fsmodel
 
 import java.nio.file.Path
+import Implicits._
 
 class TypedZ3Tests extends org.scalatest.FunSuite {
 
-  test("sanity") {
-    val z = new Z3Impl
-    //assert(z.z3true != z.z3false)
+	val z = new Z3Impl
+
+  test("Z3Bools are distinct") {
+    assert(z.z3true != z.z3false)
   }
+
+  test("Z3FileStates are distinct") {
+    assert(z.isFile != z.isDir)
+    assert(z.isFile != z.doesNotExist)
+  }
+
 }
