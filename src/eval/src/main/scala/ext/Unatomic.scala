f0ef72be76f5d431061bcc80952f35ca273203c0
Arjun Guha <arjun@cs.umass.edu>
2015-02-24 17:04:53 -0500

    fix to unatomic

--- src/eval/src/main/scala/ext/Unatomic.scala
+++ src/eval/src/main/scala/ext/Unatomic.scala
@@ -8,7 +8,7 @@ private[ext] object Unatomic {
   def unatomic(expr: Expr): Expr = expr match {
     case Seq(p, q) => unatomic(p) >> unatomic(q)
     case Alt(p, q) => unatomic(p) + unatomic(q)
-    case Atomic(p) => Atomic(unatomic(p))
+    case Atomic(p) => unatomic(p)
     case Concur(_, _) => throw new IllegalArgumentException(s"unatomic($expr)")
     case Error|Skip|Mkdir(_)|Rm(_)|Cp(_,_)|CreateFile(_, _) => expr
     case Filter(_) => expr
