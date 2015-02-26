d3ad76480cc45fb55884e0fdd70a34babf2d1b95
KaiGeffen <geffenkai@gmail.com>
2015-02-26 15:46:13 -0500

    SAT test for program installing single package runs

--- src/pipeline/src/test/scala/Z3SATTest.scala
+++ src/pipeline/src/test/scala/Z3SATTest.scala
@@ -1,21 +1,44 @@
 package pipeline
 
-import org.scalatest.FunSuite 
+// TODO(kgeffen) prune
+import puppet.syntax._
+import puppet.graph._
 
-class PackageTestSuite extends FunSuite {
+import puppet.common.{resource => resrc}
+
+import scalax.collection.Graph
+import scalax.collection.GraphEdge._
+import scalax.collection.GraphPredef._
+import scalax.collection.edge.Implicits._
+
+import fsmodel._
+import fsmodel.ext._
+//
+
+import org.scalatest.FunSuite
+import fsmodel.core.Z3Eval._
+import fsmodel.core.Z3Eval.z._
+import java.nio.file.Paths
+
+class PackageZ3TestSuite extends FunSuite {
 
   test("single package without attributes") {
     val program = """package{"sl": }"""
 
     val graph = parse(program).desugar()
                               .toGraph(Facter.run())
-    val ext_expr = resourceGraphToExpr(graph)
+    val ext_expr = pipeline.resourceGraphToExpr(graph)
 
     val core_expr = ext_expr.unconcurOpt()
                            .unatomic()
                            .toCore()
 
-    assert(Some(True) == CheckSAT(core_expr))
+    assert(Some(true) ==
+      checkSAT(evalR(core_expr,
+                     setFileState(path(Paths.get("/")),
+                                  isDir,
+                                  newState), // TODO(kgeffen) Set root to isDir
+                     newState)))
   }
 
   /*
