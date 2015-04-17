67bc70e9e734120e4163d9acdee3fd536239a157
Nimish Gupta <nimish@cs.umass.edu>
2015-04-17 11:50:18 -0400

    2 failing tests for dependency graph to fs expr

--- pipeline/src/test/scala/UnitTests.scala
+++ pipeline/src/test/scala/UnitTests.scala
@@ -19,13 +19,40 @@ class UnitTestSuite extends FunSuite {
   }
 
   test("reduce graph with concurrent operations") {
-    val a = Filter(True)
-    val b = Filter(False)
+    val a = Mkdir("/a")
+    val b = Mkdir("/b")
 
     val reduced_exp = pipeline.reduceGraph(Graph(a, b), toExpr)
 
     // both are semantically equivalent but not syntactically
     assert(((Skip >> (Atomic(a) * Atomic(b))) == reduced_exp) ||
            ((Skip >> (Atomic(b) * Atomic(a))) == reduced_exp))
+  }
+
+  test("indirect concurrency 1") {
+    val a = Mkdir("/a")
+    val b = Mkdir("/b")
+    val c = Mkdir("/c")
+    val d = Mkdir("/d")
+    val e = Mkdir("/e")
+
+    val graph = Graph(DiEdge(a, b), DiEdge(c, d), DiEdge(b, e), DiEdge(d, e))
+    val expr = pipeline.reduceGraph(graph, toExpr)
+    assert(expr == (((Atomic(a) >> Atomic(b))*
+                     (Atomic(c) >> Atomic(e))) >>
+                     Atomic(e)))
+  }
+
+  test("indirect concurrency") {
+    val a = Mkdir("/a")
+    val q = Mkdir("/q")
+    val x = Mkdir("/x")
+    val p = Mkdir("/p")
+
+    val graph = Graph(DiEdge(q, x), DiEdge(p, x), DiEdge(p, a))
+    val reduced_exp = pipeline.reduceGraph(graph, toExpr)
+    info(reduced_exp.toString)
+    // It's still not clear what expression is compiler supposed to produce
+    assert(false)
   }
 }
