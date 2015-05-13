74959f1cdbe5586a9e67d2b83aad70c51a0c0a06
Arjun Guha <arjun@cs.umass.edu>
2015-05-13 13:44:05 -0400

    Updated benchmarks to use Z3

--- pipeline/src/test/scala/BenchmarkTests.scala
+++ pipeline/src/test/scala/BenchmarkTests.scala
@@ -1,6 +1,7 @@
 import pipeline._
 import eval._
 import puppet.Facter
+import z3analysis._
 
 import org.scalatest.FunSuite
 
@@ -14,9 +15,15 @@ class BenchmarkTests extends FunSuite {
     test(s"benchmark: $name") {
 //      val expr = pipeline.resourceGraphToExpr(b.toGraph(facterEnv).head._2)
 //      val finalStates = Ample.finalStates(Ubuntu.fs, expr)
-      val graph = b.toGraph(facterEnv).head._2
-      val finalStates = AmpleGraph.finalStates(Ubuntu.fs, graph)(pipeline.GraphResourceToExpr)
-      assert(1 == finalStates.size)
+      // val graph = b.toGraph(facterEnv).head._2
+      // val finalStates = AmpleGraph.finalStates(Ubuntu.fs, graph)(pipeline.GraphResourceToExpr)
+      // assert(1 == finalStates.size)
+      val myBdd = bdd.Bdd[TestFileState]((x, y) => x < y)
+      val resourceGraph = b.toGraph(facterEnv).head._2
+      val fileScriptGraph = Slicing.sliceGraph(pipeline.toFileScriptGraph(resourceGraph))
+      info(fileScriptGraph.toString)
+      val pre = WeakestPreconditions.wpGraphBdd(myBdd)(fileScriptGraph, myBdd.bddTrue)
+      assert(Z3Evaluator.isDeterministic(myBdd)(myBdd.bddTrue, fileScriptGraph))
     }
   }
 }
