f786bff0b041ca03861e8a918097312578e0a97a
Arjun Guha <arjun@cs.umass.edu>
2015-05-14 12:21:29 -0400

    Added a top-level
    Run as follows:
    
        sbt "; project rehearsal; run"
    
    Within the top-level:
    
    import rehearsal.repl._

---
+++ rehearsal/src/main/scala/Main.scala
@@ -0,0 +1,53 @@
+package rehearsal
+
+package repl {
+
+  import scala.tools.nsc.interpreter._
+  import scala.tools.nsc.Settings
+
+  private object Main extends App {
+
+    def repl = new ILoop
+
+    val settings = new Settings
+    settings.Yreplsync.value = true
+
+    //use when launching normally outside SBT
+    //settings.usejavacp.value = true
+
+    //an alternative to 'usejavacp' setting, when launching from within SBT
+    settings.embeddedDefaults[Main.type]
+
+    repl.process(settings)
+  }
+
+}
+
+package object repl {
+
+  import pipeline._
+  import eval._
+  import pipeline.toFileScriptGraph
+  import puppet.Modules
+  import puppet.syntax.{TopLevel, parse}
+
+  val modules = Modules("benchmarks/puppetforge-modules/modules").withoutRuby
+
+  def loadModuleClass(name: String) = {
+    val likelyClassName = name.split("/").last
+    val mod = modules.loadWithDependencies(name)
+    val include = parse(s"include $likelyClassName")
+    val pp = TopLevel(mod.items ++ include.items)
+    toFileScriptGraph(pp.desugar.toGraph(puppet.Facter.emptyEnv).head._2)
+  }
+
+  def isDeterministic(g: FileScriptGraph): Boolean = {
+    val myBdd = bdd.Bdd[TestFileState]((x, y) => x < y)
+    val fileScriptGraph = Slicing.sliceGraph(g)
+    println(fileScriptGraph)
+    val pre = WeakestPreconditions.wpGraphBdd(myBdd)(fileScriptGraph, myBdd.bddTrue)
+    println(WeakestPreconditions.bddToPred(myBdd)(pre))
+    Z3Evaluator.isDeterministic(myBdd)(pre, fileScriptGraph)
+  }
+
+}
