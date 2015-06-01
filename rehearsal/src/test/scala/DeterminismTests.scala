3cda79b64450519270c97f28edcb09f136a4e84c
Nimish Gupta <nimish@cs.umass.edu>
2015-06-01 17:21:13 -0400

    Failing determinism test

--- rehearsal/src/test/scala/DeterminismTests.scala
+++ rehearsal/src/test/scala/DeterminismTests.scala
@@ -2,15 +2,16 @@ import scalax.collection.Graph
 import scalax.collection.GraphEdge.DiEdge
 import rehearsal.ppmodel._
 import rehearsal.fsmodel._
+import puppet.syntax.parse
 import puppet.graph._
 import puppet.Facter
 import Implicits._
 
-class DeterminismTestSuite extends InlineTestSuite {
+class DeterminismTestSuite extends /*InlineTestSuite*/ org.scalatest.FunSuite {
 
   def myTestRunner(g: FileScriptGraph): Boolean = {
     //SymbolicEvaluator.isErrorFree(g)
-    SymbolicEvaluator.isDeterministic(g)
+    SymbolicEvaluator.isDeterministic(g, false)
   }
 
   def genericTestRunner(resourceGraph: ResourceGraph,
@@ -18,11 +19,14 @@ class DeterminismTestSuite extends InlineTestSuite {
     SymbolicEvaluator.isDeterministic(g)
   }
 
+/*
   test("trivial program with non-deterministic error") {
     assert(myTestRunner(Graph(Mkdir("/foo"),
                               Mkdir("/foo/bar"))) == false)
   }
+*/
 
+/*
   test("trivial program with non-deterministic output") {
     assert(myTestRunner(
            Graph(If(TestFileState("/foo", IsDir), Mkdir("/bar"), Skip),
@@ -68,8 +72,55 @@ class DeterminismTestSuite extends InlineTestSuite {
 
   test("Two-node non-deterministic graph") {
     import Implicits._
-    myTestRunner(Graph(Mkdir("/foo"),
-      If(TestFileState("/foo", IsDir), Skip, Mkdir("/bar"))))
+    assert(false == myTestRunner(Graph(Mkdir("/foo"),
+      If(TestFileState("/foo", IsDir), Skip, Mkdir("/bar")))))
   }
 
+  test("a bug") {
+    val p = "/usr/games/sl"
+    val c = ""
+    val n1 = CreateFile(p, c)
+    val n2 = If(TestFileState(p, IsFile),
+                Rm(p) >> CreateFile(p, c),
+                If(TestFileState(p, DoesNotExist), CreateFile(p, c), Skip))
+    assert(false == myTestRunner(Graph(n1, n2)))
+  }
+
+  test("should be deterministic") {
+    val p = "/usr/foo"
+    val c = "c"
+    assert(true == myTestRunner(Graph(CreateFile(p, c), CreateFile(p, c))))
+  }
+
+  test("file removal and creation should be non-deterministic") {
+    val p = "/usr/foo"
+    assert(false == myTestRunner(Graph(Rm(p), CreateFile(p, ""))))
+  }
+*/
+
+  test("should be non-deterministic") {
+    val p = "/usr/foo"
+    val c1 = "abcdef"
+    val c2 = "xyz"
+    val stmt1 = If(TestFileState(p, DoesNotExist), CreateFile(p, c1), Rm(p) >> CreateFile(p, c1))
+    val stmt2 = If(TestFileState(p, DoesNotExist), CreateFile(p, c2), Rm(p) >> CreateFile(p, c2))
+    assert(false == myTestRunner(Graph(stmt1, stmt2)))
+  }
+
+
+/*
+  test("package with config file non-deterministic graph") {
+    val program = """
+      file {'/usr/games/sl': ensure => present }
+
+      package {'sl': ensure => present }
+    """
+    val pp = parse(program)
+    val g = toFileScriptGraph(pp.desugar.toGraph(Facter.emptyEnv).head._2)
+    g.nodes.foreach(n => println(n.value.pretty()))
+    val g2 = Slicing.sliceGraph(g)
+    g2.nodes.foreach(n => println(n.value.pretty()))
+    assert(false == myTestRunner(g))
+  }
+*/
 }
