7f5b919b7542e2a8ad67d8c940096e8a65d555df
Arjun Guha <arjun@cs.umass.edu>
2015-06-26 09:23:20 -0400

    explicitly set log level in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ARGS=$@
-sbt  --warn "set showSuccess in ThisBuild := false" "project rehearsal" "run $ARGS"
+sbt -Dorg.slf4j.simpleLogger.defaultLogLevel=info  --warn "set showSuccess in ThisBuild := false" "project rehearsal" "run $ARGS"
