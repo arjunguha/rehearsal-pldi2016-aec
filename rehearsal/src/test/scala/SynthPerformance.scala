030356fdd636255b10535bbc203eab657ccf7074
Aaron Weiss <aaronweiss74@gmail.com>
2015-07-01 15:23:33 -0400

    Added some timing tests for various prefix sizes.

--- rehearsal/src/test/scala/SynthPerformance.scala
+++ rehearsal/src/test/scala/SynthPerformance.scala
@@ -17,6 +17,16 @@ class SynthPerformance extends org.scalatest.FunSuite {
   println("Grow first manifest")
   println("-=-=-=-=-=-=--=-=-=")
   0.to(10).map(x => {
+    val pre = genPrefix(x)
+    val start = java.lang.System.currentTimeMillis()
+    uncurry(execLists)(gen(3) match {
+      case (a, b) =>  (pre ++ a, pre ++ b)
+    })
+    val end = java.lang.System.currentTimeMillis()
+    println(s"$x\t${end - start}")
+  })
+
+  0.to(10).map(x => {
     val start = java.lang.System.currentTimeMillis()
     uncurry(execLists)(gen(x, 0))
     val end = java.lang.System.currentTimeMillis()
