e43aa0956c677814bd20b3b02d12d3a2812d2431
jcollard <josephmcollard@gmail.com>
2015-07-01 13:49:35 -0400

    Additional test

--- rehearsal/src/test/scala/SynthPerformance.scala
+++ rehearsal/src/test/scala/SynthPerformance.scala
@@ -3,7 +3,14 @@ import rehearsal.UpdateSynth._
 class SynthPerformance extends org.scalatest.FunSuite {
   def uncurry[A, B, C](f: (A, B) => C)(t: (A, B)): C = f(t._1, t._2)
 
-  0.to(5).map(x => {
+  0.to(10).map(x => {
+    val start = java.lang.System.currentTimeMillis()
+    uncurry(execLists)(gen(1, x))
+    val end = java.lang.System.currentTimeMillis()
+    println(s"$x\t${end - start}")
+  })
+
+  0.to(3).map(x => {
     val start = java.lang.System.currentTimeMillis()
     uncurry(execLists)(gen(x))
     val end = java.lang.System.currentTimeMillis()
