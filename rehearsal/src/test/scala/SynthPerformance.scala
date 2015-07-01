e0b513c1ca4bc79bea324433a8d46239043aa168
Aaron Weiss <aaronweiss74@gmail.com>
2015-07-01 15:28:32 -0400

    Reorganized addition to match output changes for SynthPerformance.

--- rehearsal/src/test/scala/SynthPerformance.scala
+++ rehearsal/src/test/scala/SynthPerformance.scala
@@ -3,19 +3,8 @@ import rehearsal.UpdateSynth._
 class SynthPerformance extends org.scalatest.FunSuite {
   def uncurry[A, B, C](f: (A, B) => C)(t: (A, B)): C = f(t._1, t._2)
 
-  println("Grow second manifest")
+  println("Grow manifest prefix")
   println("-=-=-=-=-=--=-=-=-=-")
-
-  0.to(10).map(x => {
-    val start = java.lang.System.currentTimeMillis()
-    uncurry(execLists)(gen(0, x))
-    val end = java.lang.System.currentTimeMillis()
-    println(s"$x\t${end - start}")
-  })
-
-
-  println("Grow first manifest")
-  println("-=-=-=-=-=-=--=-=-=")
   0.to(10).map(x => {
     val pre = genPrefix(x)
     val start = java.lang.System.currentTimeMillis()
@@ -26,6 +15,17 @@ class SynthPerformance extends org.scalatest.FunSuite {
     println(s"$x\t${end - start}")
   })
 
+  println("Grow second manifest")
+  println("-=-=-=-=-=--=-=-=-=-")
+  0.to(10).map(x => {
+    val start = java.lang.System.currentTimeMillis()
+    uncurry(execLists)(gen(0, x))
+    val end = java.lang.System.currentTimeMillis()
+    println(s"$x\t${end - start}")
+  })
+
+  println("Grow first manifest")
+  println("-=-=-=-=-=-=--=-=-=")
   0.to(10).map(x => {
     val start = java.lang.System.currentTimeMillis()
     uncurry(execLists)(gen(x, 0))
