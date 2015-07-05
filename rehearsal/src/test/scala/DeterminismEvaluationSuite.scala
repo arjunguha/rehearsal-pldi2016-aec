3f8a71b56c10fe8fb3b53a49fb48894dab1df2c5
Arjun Guha <arjun@cs.umass.edu>
2015-07-05 19:19:14 -0400

    Seven benchmarks

--- rehearsal/src/test/scala/DeterminismEvaluationSuite.scala
+++ rehearsal/src/test/scala/DeterminismEvaluationSuite.scala
@@ -41,4 +41,24 @@ class DeterminismEvaluationSuite extends org.scalatest.FunSuite {
     assert(SymbolicEvaluator.isDeterministic(Slicing.sliceGraph(g)) == false)
   }
 
+  test("ghoneycutt-xinetd.json") {
+    val rg = Catalog.parseFile(s"$root/ghoneycutt-xinetd.json")
+    val g = toFileScriptGraph(rg)
+    assert(SymbolicEvaluator.isDeterministic(g) == false)
+  }
+
+  test("thias-ntp.json") {
+    val rg = Catalog.parseFile(s"$root/thias-ntp.json")
+    val g = toFileScriptGraph(rg)
+    assert(SymbolicEvaluator.isDeterministic(g) == false)
+  }
+
+  test("xdrum-rsyslog.json") {
+    val rg = Catalog.parseFile(s"$root/xdrum-rsyslog.json")
+    val g = toFileScriptGraph(rg)
+    assert(SymbolicEvaluator.isDeterministic(g) == false)
+  }
+
+
+
 }
