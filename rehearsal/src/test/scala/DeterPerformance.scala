8b631e008c350f103bb6c5d0421b5a185e29582d
Rian Shambaugh <rolivia@gwmail.gwu.edu>
2015-07-08 02:10:38 -0400

    better idea for determinism benchmark...getting out of bounds error. not sure why

--- rehearsal/src/test/scala/DeterPerformance.scala
+++ rehearsal/src/test/scala/DeterPerformance.scala
@@ -2,14 +2,15 @@ import rehearsal._
 import FSSyntax._
 import scala.util.Random
 import java.nio.file.Paths
+import java.nio.file.Path
 import scalax.collection.Graph
 import scalax.collection.GraphEdge.DiEdge
 
 
 class DeterPerformance extends org.scalatest.FunSuite {
-  val maxPathComponentLen = 10
+  val maxPathComponentLen = 3
   val maxFileLen = 10
-  val paths = Seq()
+  var paths: List[Path] = List()
 
   def randAlphaNumString(length: Int) = {
     val rand = new Random()
@@ -28,28 +29,50 @@ class DeterPerformance extends org.scalatest.FunSuite {
     for(i <- 1 to pathLen-1){
       path += "/" + randAlphaNumString(maxPathComponentLen)
     }
-    paths = path :: paths
+    paths = paths.:::(List(Paths.get(path)))
     Paths.get(path)
   }
 
-  def genRandomProg(length: Int, maxPathLen: Int) = {
-    var x = List(CreateFile(genRandomPath(maxPathLen), randAlphaNumString(maxFileLen)))
+  def getRandomPath() = {
+    val rand = new Random()
+    val i = rand.nextInt(paths.length - 1)
+    if(i ==0){
+      paths = paths.drop(i)
+    }else if(i == paths.length-1){
+      paths = paths.dropRight(i)
+    }else{
+      paths = paths.dropRight(i) ++ paths.drop(i)
+    }    
+    paths(i)
+  }
 
-    for(i <- 2 to length){
-      x = x.:::(List(CreateFile(genRandomPath(maxPathLen), randAlphaNumString(maxFileLen))))
+  def genRandomProg(length: Int, pathLen: Int) = {
+    var x: List[Expr] = List()
+
+    for(i <- 1 to length){
+      val path = getRandomPath()
+      x = x.:::(List(CreateFile(path, randAlphaNumString(maxFileLen))))
     }
 
     x.foldRight(Skip: Expr)((e, expr) => FSSyntax.Seq(e, expr))
   }
 
-  def genRandomGraph(numProgs: Int, progLength: Int, maxPathLen: Int, overlap: Int) = {
-    val g = Graph[Expr, DiEdge]()
+  def genRandomGraph(numProgs: Int, progLength: Int, pathLen: Int, overlap: Int) = {
+    val numPaths = numProgs * progLength - overlap;
+    for(i <- 1 to numPaths){
+      genRandomPath(pathLen)
+    }
+    for(j <- 1 to overlap){
+      paths = paths.:::(List(paths(new Random().nextInt(paths.length))))
+    }
+    var g = Graph[Expr, DiEdge]()
     for(i <- 1 to numProgs){
-      g.add(genRandomProg(progLength, maxPathLen))
+      g = g + (genRandomProg(progLength, pathLen))
     }
     g
   }
 
   val rand = new Random()
-  println(genRandomGraph(rand.nextInt(10), rand.nextInt(10), 10))
+  genRandomGraph(10, 3, 3, 2)
+  println(paths)
 }
