bcafdfb24be054506b10075a742f0b8e61f40766
Aaron Weiss <aaronweiss74@gmail.com>
2015-08-05 17:03:43 -0400

    Added resource type definitions to external AST.

--- parser/src/main/scala/Syntax.scala
+++ parser/src/main/scala/Syntax.scala
@@ -2,6 +2,7 @@ package parser
 
 object Syntax {
   case class Attribute(name: String, value: Atom)
+  case class Argument(id: String, typ: String, default: Option[Atom])
 
   sealed trait Atom
   case class ASymbol(name: String) extends Atom
@@ -13,4 +14,5 @@ object Syntax {
   case class Resource(name: String, typ: String, attributes: Seq[Attribute]) extends Expr
   case class LeftEdge(parent: String, child: String) extends Expr
   case class RightEdge(parent: String, child: String) extends Expr
+  case class Define(name: String, args: Seq[Argument], body: Seq[Expr]) extends Expr
 }
