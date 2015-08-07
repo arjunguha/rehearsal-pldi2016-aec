821139136ebcedc49ec33ea41cfde6a83ede1737
Aaron Weiss <aaronweiss74@gmail.com>
2015-08-07 13:29:00 -0400

    Updated Internal to bring it in line with changes to Syntax.

--- rehearsal/src/main/scala/Parser/Internal.scala
+++ rehearsal/src/main/scala/Parser/Internal.scala
@@ -9,19 +9,20 @@ object Internal {
   case class ABool(value: Boolean) extends Atom
   case class AString(value: String) extends Atom
   case class AVar(id: String) extends Atom
+  case class ARes(typ: String, id: String) extends Atom
 
   sealed trait BoolOps
-  case class BAtom (atom: Atom) extends BoolOps
+  case class BAtom(atom: Atom) extends BoolOps
   case class BNAnd(lhs: BoolOps, rhs: BoolOps) extends BoolOps
-  case class BEq (lhs: BoolOps, rhs: BoolOps) extends BoolOps
-  case class BNEq (lhs: BoolOps, rhs: BoolOps) extends BoolOps
-  case class BMatch (lhs: BoolOps, rhs: BoolOps) extends BoolOps
-  case class BNMatch (lhs: BoolOps, rhs: BoolOps) extends BoolOps
-  case class BIn (lhs: BoolOps, rhs: BoolOps) extends BoolOps
+  case class BEq(lhs: BoolOps, rhs: BoolOps) extends BoolOps
+  case class BNEq(lhs: BoolOps, rhs: BoolOps) extends BoolOps
+  case class BMatch(lhs: BoolOps, rhs: BoolOps) extends BoolOps
+  case class BNMatch(lhs: BoolOps, rhs: BoolOps) extends BoolOps
+  case class BIn(lhs: BoolOps, rhs: BoolOps) extends BoolOps
 
   sealed trait Expr
   case class Resource(name: String, typ: String, attributes: Seq[Attribute]) extends Expr
-  case class Edge(parent: String, child: String) extends Expr
+  case class Edge(parent: ARes, child: ARes) extends Expr
   case class Define(name: String, args: Seq[Argument], body: Seq[Expr]) extends Expr
-  case class ITE(pred: BoolOps, lhs: Seq[Expr], rhs: Seq[Expr]) extends Expr
+  case class ITE(pred: BoolOps, thn: Seq[Expr], els: Option[Seq[Expr]]) extends Expr
 }
