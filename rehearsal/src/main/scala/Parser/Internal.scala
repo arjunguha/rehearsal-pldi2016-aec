f7cf6454916145d7b48dd42ecc50855a452d92ad
Aaron Weiss <aaronweiss74@gmail.com>
2015-08-12 12:19:39 -0400

    Added Block and EmptyExpr to Internal syntax, and updated AST elements.

--- rehearsal/src/main/scala/Parser/Internal.scala
+++ rehearsal/src/main/scala/Parser/Internal.scala
@@ -21,31 +21,34 @@ object Internal {
   case class BIn(lhs: BoolOps, rhs: BoolOps) extends BoolOps
 
   sealed trait Expr
+  case object EmptyExpr extends Expr
+  case class Block(e1: Expr, e2: Expr) extends Expr
   case class Resource(id: Atom, typ: String, attributes: Seq[Attribute]) extends Expr
   case class Edge(parent: ARes, child: ARes) extends Expr
-  case class Define(name: String, args: Seq[Argument], body: Seq[Expr]) extends Expr
-  case class ITE(pred: BoolOps, thn: Seq[Expr], els: Option[Seq[Expr]]) extends Expr
-  case class Class(name: String, parameters: Seq[Argument], body: Seq[Expr]) extends Expr
+  case class Define(name: String, args: Seq[Argument], body: Expr) extends Expr
+  case class ITE(pred: BoolOps, thn: Expr, els: Expr) extends Expr
+  case class Class(name: String, parameters: Seq[Argument], body: Expr) extends Expr
 
-  def simplifyAttributes(lst: Seq[Attribute], src: ARes): (Seq[Attribute], Seq[Expr]) = 
-  	lst.foldRight[(Seq[Attribute], Seq[Expr])]((Seq(), Seq())) {
-  		case (Attribute("before", res@ARes(_, _)), (attrs, exprs)) => (attrs, Edge(src, res) +: exprs)
-  		case (Attribute("require", res@ARes(_, _)), (attrs, exprs)) => (attrs, Edge(res, src) +: exprs)
-  		case (attr, (attrs, exprs)) => (attr +: attrs, exprs)
-  	}
+  def simplifyAttributes(lst: Seq[Attribute], src: ARes): (Seq[Attribute], Expr) = {
+    lst.foldRight[(Seq[Attribute], Expr)]((Seq(), EmptyExpr)) {
+      case (Attribute("before", res@ARes(_, _)), (attrs, expr)) => (attrs, Block(Edge(src, res), expr))
+      case (Attribute("require", res@ARes(_, _)), (attrs, expr)) => (attrs, Block(Edge(res, src), expr))
+      case (attr, (attrs, expr)) => (attr +: attrs, expr)
+    }
+  }
 
-  def desugar(lst: Seq[Expr]): Seq[Expr] = 
-  	lst.foldRight[Seq[Expr]](Seq()) {
-  		case (Resource(AString(id), typ, attrs), acc) => simplifyAttributes(attrs, ARes(typ.capitalize, id)) match {
-  			case (attrs, exprs) => Resource(AString(id), typ, attrs) +: (exprs ++ acc)
-  		}
-  		case (Resource(id, typ, attrs), acc) => simplifyAttributes(attrs, ARes(typ.capitalize, "__" + id.toString + "__")) match {
-  			case (attrs, exprs) => Resource(id, typ, attrs) +: (exprs ++ acc)
-  		}
-  		case (Define(name, args, body), acc) => Define(name, args, desugar(body)) +: acc
-  		case (ITE(pred, thn, els), acc) => ITE(pred, desugar(thn), els.map(desugar)) +: acc
-  		case (Class(name, parameters, body), acc) => Class(name, parameters, desugar(body)) +: acc
-  		case (edge@Edge(_, _), acc) => edge +: acc 
-  	}
-
+  def desugar(expr: Expr): Expr = expr match {
+    case EmptyExpr => EmptyExpr
+    case Block(e1, e2) => Block(desugar(e1), desugar(e2))
+    case Resource(AString(id), typ, attrs) => simplifyAttributes(attrs, ARes(typ.capitalize, id)) match {
+      case (attrs, expr) => Block(Resource(AString(id), typ, attrs), expr)
+    }
+    case Resource(id, typ, attrs) => simplifyAttributes(attrs, ARes(typ.capitalize, "__" + id.toString + "__")) match {
+      case (attrs, expr) => Block(Resource(id, typ, attrs), expr)
+    }
+    case Define(name, args, body) => Define(name, args, desugar(body))
+    case ITE(pred, thn, els) => ITE(pred, desugar(thn), desugar(els))
+    case Class(name, parameters, body) => Class(name, parameters, desugar(body))
+    case edge@Edge(_, _) => edge
+  }
 }
