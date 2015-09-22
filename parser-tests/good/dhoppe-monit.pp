abf03143bf3c5180311b4106b0d7c65e80ea32fc
Arjun Guha <arjun@cs.umass.edu>
2015-09-22 16:01:43 -0400

    Added benchmark to parser test cases

---
+++ parser-tests/good/dhoppe-monit.pp
@@ -0,0 +1,81 @@
+// From https://github.com/dhoppe/puppet-monit
+define monit::service() {
+  file { "/etc/monit/conf.d/${name}":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => "puppet:///modules/monit/common/etc/monit/conf.d/${name}",
+    notify  => Service['monit'],
+    require => [
+      File['conf.d'],
+      Package['monit']
+    ],
+  }
+}
+
+class monit::params {
+  case $::lsbdistcodename {
+    'squeeze': {
+    }
+    default: {
+      fail("Module ${module_name} does not support ${::lsbdistcodename}")
+    }
+  }
+}
+
+class monit inherits monit::params {
+  file { '/etc/default/monit':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    alias   => 'monit',
+    source  => "puppet:///modules/monit/${::lsbdistcodename}/etc/default/monit",
+    notify  => Service['monit'],
+    require => Package['monit'],
+  }
+
+  file { '/etc/monit/conf.d':
+    ensure  => directory,
+    force   => true,
+    purge   => true,
+    recurse => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    alias   => 'conf.d',
+    require => Package['monit'],
+  }
+
+  file { '/etc/monit/monitrc':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    alias   => 'monitrc',
+    content => template("monit/${::lsbdistcodename}/etc/monit/monitrc.erb"),
+    notify  => Service['monit'],
+    require => Package['monit'],
+  }
+
+  package { 'monit':
+    ensure => present,
+  }
+
+  service { 'monit':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => false,
+    require    => [
+      File['monit'],
+      File['monitrc'],
+      Package['monit']
+    ],
+  }
+}
+
+class monit::disabled inherits monit {
+  Service['monit'] {
+    ensure => stopped,
+    enable => false,
+  }
+}
