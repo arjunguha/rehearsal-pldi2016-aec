aa7c279e29597d613eff4579f09ed13dd5f40aa6
Aaron Weiss <aaronweiss74@gmail.com>
2015-09-29 10:40:57 -0400

    Removed usage of node default in SpikyIRC manifest.

--- parser-tests/good/nfisher-SpikyIRC.pp
+++ parser-tests/good/nfisher-SpikyIRC.pp
@@ -1,24 +1,24 @@
 # From https://github.com/nfisher/SpikyIRC
+# Contents were flattened by removing the usage of node.
 # Drugs are baaadd m'kay. Don't do stages at home kids.
 stage { 'epel': before => Stage['main'] }
 
-node default {
-  include apache
-  include collectd
-  include collectd::web
-  class { 'epel': stage => 'epel' }
-  include ircd
-  include irssi
-  include ssh
-  include users
-  include sudoers
+include apache
+include collectd
+include collectd::web
+class { 'epel': stage => 'epel' }
+include ircd
+include irssi
+include ssh
+include users
+include sudoers
 
-  # TODO: This is a pants way to manage what's run against Vagrant.
-  if "localhost.localdomain" != $::fqdn {
-  include postfix
-  include sudoers
-  }
+# TODO: This is a pants way to manage what's run against Vagrant.
+if "localhost.localdomain" != $::fqdn {
+include postfix
+include sudoers
 }
+
 class apache {
   package { 'httpd':
     ensure => latest,
