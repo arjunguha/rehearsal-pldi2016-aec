f645674812cf1f56ad7e975b770de958bf26d535
rolivia <rian@cs.umass.edu>
2015-09-30 16:57:09 -0400

    fixed global namespace parsing issue

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -7,19 +7,19 @@ case class ParseError(msg: String) extends RuntimeException(msg)
 
 private class Parser extends RegexParsers with PackratParsers{
 
-  import Implicits._
+	import Implicits._
 
-  type P[T] = PackratParser[T]
+	type P[T] = PackratParser[T]
 
-  override protected val whiteSpace = """(\s|#.*|(/\*((\*[^/])|[^*])*\*/))+""".r
+	override protected val whiteSpace = """(\s|#.*|(/\*((\*[^/])|[^*])*\*/))+""".r
 
 
-  // TODO(arjun): escape sequences? interpolation?
+	// TODO(arjun): escape sequences? interpolation?
 	lazy val stringVal: P[String] =
-	  "\"" ~> "[^\"]*".r <~ "\"" |
+		"\"" ~> "[^\"]*".r <~ "\"" |
 		"'" ~> "[^']*".r <~ "'"
 	// TODO: is there a better way to write this?
-	lazy val word: P[String] = "([a-zA-Z]+(::)?[a-zA-Z]+)+|[a-zA-Z]+".r ^^ { case x => x }
+	lazy val word: P[String] = "(::)?([a-zA-Z]+(::)?[a-zA-Z]+)+|[a-zA-Z]+".r ^^ { case x => x }
 	lazy val id: P[String] = "" ~> "[a-z_][a-zA-Z0-9_]*".r
 	lazy val attributeName: P[String] = "" ~> "[a-z]+".r
 	lazy val dataType: P[String] = "" ~> "[A-Z][a-zA-Z]+".r
@@ -27,32 +27,31 @@ private class Parser extends RegexParsers with PackratParsers{
 
 	//Manifest
 	lazy val manifest: P[Manifest] =
-	  edge |
-	  let |
-	  define |
-	  resource |
-	  classManifest |
-          caseManifest |
-	  ite |
-	  exprMan
-
+		edge |
+		let |
+		define |
+		resource |
+		classManifest |
+		caseManifest |
+		ite |
+		exprMan
 
 	lazy val body: P[Manifest] = "{" ~> prog <~ "}"
 
-        lazy val classManifest: P[Manifest] =
-          ("class" ~> word) ~ opt(parameters) ~ opt("inherits" ~> word) ~ body ^^ {
-            case x ~ Some(params) ~ y ~ m => Class(x, params, y, m)
-            case x ~ None ~ y ~ m => Class(x, Seq(), y, m)
-          }
+	lazy val classManifest: P[Manifest] =
+		("class" ~> word) ~ opt(parameters) ~ opt("inherits" ~> word) ~ body ^^ {
+			case x ~ Some(params) ~ y ~ m => Class(x, params, y, m)
+			case x ~ None ~ y ~ m => Class(x, Seq(), y, m)
+		}
 
-        lazy val caseManifest: P[Manifest] = "case" ~ expr ~ "{" ~ cases ~ "}" ^^ {
-          case _ ~ e ~ _ ~ lst ~ _ => MCase(e, lst)
-        }
+	lazy val caseManifest: P[Manifest] = "case" ~ expr ~ "{" ~ cases ~ "}" ^^ {
+		case _ ~ e ~ _ ~ lst ~ _ => MCase(e, lst)
+	}
 
 	lazy val cases: P[Seq[Case]] =
-	  "default" ~ ":" ~ body ^^ { case _ ~ _ ~ m => Seq(CaseDefault(m)) } |
-	  expr ~ ":" ~ body ~ cases ^^ { case e ~ _ ~ m ~ rest => CaseExpr(e, m) +: rest } |
-	  success(()) ^^ { case _ => Seq[Case]() }
+		"default" ~ ":" ~ body ^^ { case _ ~ _ ~ m => Seq(CaseDefault(m)) } |
+		expr ~ ":" ~ body ~ cases ^^ { case e ~ _ ~ m ~ rest => CaseExpr(e, m) +: rest } |
+		success(()) ^^ { case _ => Seq[Case]() }
 
 	lazy val parameter: P[Argument] = opt(dataType) ~ varName ~ opt("=" ~> expr) ^^ {
 		case typ ~ id ~ Some(default) => Argument(id, Some(default))
@@ -62,18 +61,18 @@ private class Parser extends RegexParsers with PackratParsers{
 	lazy val parameters: P[Seq[Argument]] = ("(" ~> repsep(parameter, ",") <~ opt(",")) <~ ")"
 
 	lazy val resource: P[Manifest] = word ~ ("{" ~> rep1sep(resourcePair, ";") <~ "}") ^^ {
-	  case typ ~ Seq((id, attr)) => Resource(id, typ, attr)
-          case typ ~ pairs => pairs.foldRight[Manifest](Empty) {
-            case ((id, attr), m) => Block(Resource(id, typ, attr), m)
-          }
+		case typ ~ Seq((id, attr)) => Resource(id, typ, attr)
+		case typ ~ pairs => pairs.foldRight[Manifest](Empty) {
+			case ((id, attr), m) => Block(Resource(id, typ, attr), m)
+		}
 	}
 
-        lazy val resourcePair: P[(Expr, Seq[Attribute])] = (expr <~ ":") ~ attributes ^^ {
-          case id ~ attr => (id, attr)
-        }
+	lazy val resourcePair: P[(Expr, Seq[Attribute])] = (expr <~ ":") ~ attributes ^^ {
+		case id ~ attr => (id, attr)
+	}
 
-  // TODO(arjun): bop won't scale as exprs start to grow. Just write a typechecker
-  // eventually.
+	// TODO(arjun): bop won't scale as exprs start to grow. Just write a typechecker
+	// eventually.
 	lazy val ite: P[Manifest] = "if" ~> bop ~ body ~ rep(elsif) ~ opt("else" ~> body) ^^ {
 		case pred ~ thn ~ elsifs ~ els => ITE(pred, thn, elsifs.foldRight(els.getOrElse(Empty)) {
 			case ((pred, body), acc) => ITE(pred, body, acc)
@@ -108,33 +107,33 @@ private class Parser extends RegexParsers with PackratParsers{
 
 	lazy val res: P[Expr] = word ~ ("[" ~> expr <~ "]") ^^ { case typ ~ e => Res(typ, e) }
 
-        lazy val array: P[Expr] = "[" ~> repsep(expr, ",") <~ "]" ^^ { case es => Array(es) }
+	lazy val array: P[Expr] = "[" ~> repsep(expr, ",") <~ "]" ^^ { case es => Array(es) }
 
 	lazy val vari: P[Expr] = varName ^^ (Var(_))
 
 	//Operators
 	lazy val atom: P[Expr] =
-	  bool |
-	  res |
-	  vari |
-	  string |
-	  word ~ "(" ~ repsep(bop, ",") ~ ")" ^^ { case f ~ _ ~ xs ~ _  => App(f, xs) } |
-	  "(" ~ bop ~ ")" ^^ { case _ ~ e ~ _ => e }
+		bool |
+		res |
+		vari |
+		string |
+		word ~ "(" ~ repsep(bop, ",") ~ ")" ^^ { case f ~ _ ~ xs ~ _  => App(f, xs) } |
+		"(" ~ bop ~ ")" ^^ { case _ ~ e ~ _ => e }
 
 	lazy val not: P[Expr] =
-	  "!" ~> not ^^ { Not(_) } |
-	  atom
+		"!" ~> not ^^ { Not(_) } |
+		atom
 
 	lazy val and: P[Expr] =
-	  and ~ ("and" ~> not) ^^ { case lhs ~ rhs => And(lhs, rhs) } |
-	  not
+		and ~ ("and" ~> not) ^^ { case lhs ~ rhs => And(lhs, rhs) } |
+		not
 
 	lazy val or: P[Expr] =
-	  or ~ ("or" ~> and) ^^ { case lhs ~ rhs => Or(lhs, rhs) } |
-	  and
+		or ~ ("or" ~> and) ^^ { case lhs ~ rhs => Or(lhs, rhs) } |
+		and
 
 	lazy val bop: P[Expr] =
-	  bop ~ ("==" ~> or) ^^ { case lhs ~ rhs => Eq(lhs, rhs) } |
+		bop ~ ("==" ~> or) ^^ { case lhs ~ rhs => Eq(lhs, rhs) } |
 		bop ~ ("!=" ~> or) ^^ { case lhs ~ rhs => Not(Eq(lhs, rhs)) } |
 		bop ~ ("=~" ~> or) ^^ { case lhs ~ rhs => Match(lhs, rhs) } |
 		bop ~ ("!~" ~> or) ^^ { case lhs ~ rhs => Not(Match(lhs, rhs)) } |
@@ -176,14 +175,14 @@ private class Parser extends RegexParsers with PackratParsers{
 		case MCase(e, lst) => {
 			// TODO(arjun): should we ensure that "e" is only evaluted once? Does it
 			// matter? (No side-effects, right?)
-      lst.foldRight[Manifest](Empty) {
-      	// TODO(arjun): Eq(e, v) is not quite right. It won't work for
-      	// values that reduce to regular expressions. See this:
-      	// https://docs.puppetlabs.com/puppet/latest/reference/lang_conditional.html#case-matching
-      	case (CaseExpr(v, m), rest) => ITE(Eq(e, v), m, rest)
-      	case (CaseDefault(m), Empty) => m
-      	case (CaseDefault(_), _) => throw new Exception("default is not the last case (should have been caught by the grammar)")
-      }
+			lst.foldRight[Manifest](Empty) {
+				// TODO(arjun): Eq(e, v) is not quite right. It won't work for
+				// values that reduce to regular expressions. See this:
+				// https://docs.puppetlabs.com/puppet/latest/reference/lang_conditional.html#case-matching
+				case (CaseExpr(v, m), rest) => ITE(Eq(e, v), m, rest)
+				case (CaseDefault(m), Empty) => m
+				case (CaseDefault(_), _) => throw new Exception("default is not the last case (should have been caught by the grammar)")
+			}
 		}
 	}
 
@@ -191,8 +190,8 @@ private class Parser extends RegexParsers with PackratParsers{
 	lazy val prog: P[Manifest] = rep(manifest) ^^ { case exprs => blockExprs(exprs) }
 
 	def blockExprs(exprs: Seq[Manifest]): Manifest = {
-    exprs.foldRight[Manifest](Empty) { case (m1, m2) => m1 >> m2 }
-  }
+		exprs.foldRight[Manifest](Empty) { case (m1, m2) => m1 >> m2 }
+	}
 	def parseString[A](expr: String, parser: Parser[A]): A = {
 		parseAll(parser, expr) match{
 			case Success(r, _) => r
