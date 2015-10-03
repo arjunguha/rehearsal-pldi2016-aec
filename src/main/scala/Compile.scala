8da3cc1c14fe2242353699691511a8f6fda4377f
Rian Shambaugh <rolivia@gwmail.gwu.edu>
2015-10-03 13:46:02 -0400

    re-start compiler to FS; will compile to ResourceModel.Res and use compile function in ResourceModel to get to FS

--- src/main/scala/Compile.scala
+++ src/main/scala/Compile.scala
@@ -2,54 +2,37 @@ package rehearsal
 
 object Compile {
 
-	import java.nio.file.{Path, Paths}
-	import rehearsal.{FSSyntax => F}
-	import FSSyntax.{True, False, Expr, Not, Or, Block, Skip}
-	import rehearsal.Implicits._
-	import rehearsal.{Syntax => P}
-	import Syntax._
+  import java.nio.file.{Path, Paths}
+  import rehearsal.{FSSyntax => F}
+  import F.{Seq => FSeq}
+  import F._
+  import rehearsal.Implicits._
+  import rehearsal.{Syntax => P}
+  import P._
+  import ResourceModel.{Res => ResModel, File, Package, User, Group}
+  import Evaluator.ResourceGraph
+  import scalax.collection.mutable.Graph
+  import scalax.collection.mutable.Graph._
+  import scalax.collection.GraphEdge._
 
-	case class FSCompileError(msg: String) extends RuntimeException(msg)
+  case class FSCompileError(msg: String) extends RuntimeException(msg)
 
-	//Must evaluate before compiling??
+  //compile to ResourceModel.Res
 
-
-	//TODO: make this work
-	//PROBLEM: some of these should compile to Pred, others to F.Expr
-
-	// def compileExpr(e: P.Expr): F.Expr = e match {
-	// 	case Str(s) => throw FSCompileError("NYI") //??
-	// 	case Res(typ, e) => throw FSCompileError("NYI") //??
-	// 	case Bool(true) => True
-	// 	case Bool(false) => False
-	// 	case P.Not(e) => F.Not(compileExpr(e))
-	// 	case P.And(e1, e2) => F.Not(compileExpr(e1), compileExpr(e2))
-	// 	case P.Or(e1, e2) => F.Or(compileExpr(e1), compileExpr(e2))
-	// 	case Eq(e1, e2) => throw FSCompileError("NYI") //??
-	// 	case Match(e1, e2) => throw FSCompileError("NYI") //??
-	// 	case In(e1, e2) => throw FSCompileError("NYI") //??
-	// 	case Array(_) => throw FSCompileError("NYI") //??
-
-	// 	case Var(_) => throw FSCompileError("Var: not a value")
-	// 	case App(_, _) => throw FSCompileError("App: not a value")
-	// }
-
-	//TODO: fill this in
-	def compileResource(title: P.Expr, typ: String, attrs: Seq[Attribute]) = Skip
-
-  def compile(m: Manifest): F.Expr = m match {
-    case Empty => Skip
-    case P.Block(m1, m2) => F.Block(compile(m1), compile(m2))
-    case Resource(title, typ, attrs) => compileResource(title, typ, attrs)
-    case Edge(m1, m2) => Skip //TODO: figure out what to do with edges
-    //TODO: fix this
-    case E(e) => throw FSCompileError("NYI") //compileExpr(e)
-    case Define(name, params, body) => throw FSCompileError("Define: not a value")
-    case Class(name, params, inherits, body) => throw FSCompileError("Class: should have been desugared")
-    case Let(varName, e, body) => throw FSCompileError("Let: not a value")
-    case MCase(e, cases) => throw FSCompileError("Case statement: should have been desugared")
-    case Include(_) => throw FSCompileError("Include statements: should have been evaluated away")
-    case Require(_) => throw FSCompileError("Include statements: should have been evaluated away")
+  //these are the file cuttently types accepted by isPrimitiveType in PuppetEval
+  def compileResource(r: Resource): ResModel = r.typ match {
+    //TODO
+    case "file" => File(Paths.get(""), "", false)
+    //TODO
+    case "package" => Package("", false)
+    //TODO
+    case "user" => User("", false, false)
+    //TODO
+    case "group" => Group("", false)
   }
+  
 
+  //TODO(rian)
+  //use compile function in ResourceModel to go from ResModel to FS
+  def compile(g: ResourceGraph): FileScriptGraph = Graph()
 }
