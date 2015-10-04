a172278cca62201db9007c184d7bc0f305c5c802
Rian Shambaugh <rolivia@gwmail.gwu.edu>
2015-10-03 21:07:07 -0400

    finished compileResource (not tested)

--- src/main/scala/Compile.scala
+++ src/main/scala/Compile.scala
@@ -9,7 +9,7 @@ object Compile {
   import rehearsal.Implicits._
   import rehearsal.{Syntax => P}
   import P._
-  import ResourceModel.{Res => ResModel, File, Package, User, Group}
+  import ResourceModel.{Res => ResModel, File, Package, User, Group, EnsureFile, AbsentPath}
   import Evaluator.ResourceGraph
   import scalax.collection.mutable.Graph
   import scalax.collection.mutable.Graph._
@@ -28,32 +28,65 @@ object Compile {
   //these are the file cuttently types accepted by isPrimitiveType in PuppetEval
   def compileResource(r: Resource): ResModel = {
     val attrsMap = attrsToMap(r.attrs)
+    val nameExpr = attrsMap.get("name")
+    val ensureExpr = attrsMap.get("ensure")
+    val name: String = nameExpr match {
+      case None => r.title match {
+        case Str(n) => n
+        case _ => throw FSCompileError(s"invalid value for resource title: $r.title")
+      }          
+      case Some(Str(n)) => n
+      case _ => throw FSCompileError(s"invalid value for 'name' attribute: $nameExpr")
+    }
+    val present = ensureExpr match {
+      case Some(Str("present")) => true
+      case Some(Str("absent")) => false
+      //TODO: find out if there is a default value for 'ensure'
+      case _ => throw FSCompileError(s"invalid value for 'ensure' attribute: $ensureExpr")
+    }
     r.typ match {
       //TODO
-      case "file" => File(Paths.get(""), "", false)
-      //TODO
-      case "package" => Package("", false)
-      //TODO
-      case "user" => User("", false, false)
-      case "group" => {
-        val nameAttr = attrsMap.get("name")
-        val ensureAttr = attrsMap.get("ensure")
-        val name: String = nameAttr match {
+      //assusming the only difference between File and EnsureFile is that File does not have ensure attribute
+      case "file" => {
+        val pathExpr = attrsMap.get("path")
+        val contentExpr = attrsMap.get("content")
+        val forceExpr = attrsMap.get("force")
+        val path: String = pathExpr match {
           case None => r.title match {
             case Str(n) => n
             case _ => throw FSCompileError(s"invalid value for resource title: $r.title")
           }          
           case Some(Str(n)) => n
-          case _ => throw FSCompileError(s"invalid value for 'name' attribute: $nameAttr")
+          case _ => throw FSCompileError(s"invalid value for 'path' attribute: $pathExpr")
         }
-        val present = ensureAttr match {
-          case Some(Str("present")) => true
-          case Some(Str("absent")) => false
-          //TODO: find out if there is a default value for 'ensure'
-          case _ => throw FSCompileError(s"invalid value for 'ensure' attribute: $ensureAttr")
+        val content: String = contentExpr match {
+          case None => ""        
+          case Some(Str(n)) => n
+          case _ => throw FSCompileError(s"invalid value for 'content' attribute: $contentExpr")
         }
-        Group(name, present)
+        val force = forceExpr match {
+          case Some(Str("yes")) | Some(Bool(true)) => true
+          case Some(Str("no")) | Some(Bool(false)) | None => false
+          case _ => throw FSCompileError(s"invalid value for 'force' attribute: $forceExpr")
+        }
+        ensureExpr match {
+          case None => File(Paths.get(path), content, force)
+          case Some(Str("present")) => EnsureFile(Paths.get(path), content)
+          case Some(Str("absent")) => AbsentPath(Paths.get(path), force)
+          case _ => throw FSCompileError(s"invalid value for 'ensure' attribute $ensureExpr")
+        }
       }
+      case "package" => Package(name, present)
+      case "user" => {
+        val manageHomeExpr = attrsMap.get("managehome")
+        val manageHome = manageHomeExpr match {
+          case Some(Str("yes")) | Some(Bool(true)) => true
+          case Some(Str("no")) | Some(Bool(false)) | None => false
+          case _ => throw FSCompileError(s"invalid value for 'managehome' attribute: $manageHomeExpr")
+        }        
+        User(name, present, manageHome)
+      }
+      case "group" => Group(name, present)
     }
   }
   
