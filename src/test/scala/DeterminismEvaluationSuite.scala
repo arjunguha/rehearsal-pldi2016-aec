6f3cfc36864a8d04a5b76b9234efceb81da3d32a
Arjun Guha <arjun@cs.umass.edu>
2015-10-09 14:51:35 -0400

    Ported old tests to new code. Only dhoppe-monit works right now.

--- src/test/scala/DeterminismEvaluationSuite.scala
+++ src/test/scala/DeterminismEvaluationSuite.scala
@@ -1,60 +1,51 @@
 class DeterminismEvaluationSuite extends org.scalatest.FunSuite {
 
   import rehearsal._
-  import java.nio.file._
-  import Evaluator._
+  import PuppetParser2.parseFile
 
   val root = "parser-tests/good"
 
   test("dhoppe-monit.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/dhoppe-monit.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/dhoppe-monit.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministicError(g) == true)
   }
 
   test("thias-bind.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/thias-bind.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/thias-bind.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(g) == false)
   }
 
   //not in parser-tests yet
   ignore("puppet-hosting.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/puppet-hosting.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/puppet-hosting.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(g) == false)
   }
 
   test("antonlingstrom-powerdns.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/antonlingstrom-powerdns.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/antonlingstrom-powerdns.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(g) == false)
   }
 
   test("nfisher-SpikyIRC.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/nfisher-SpikyIRC.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/nfisher-SpikyIRC.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(Slicing.sliceGraph(g)) == false)
   }
 
   //not in parser-tests yet
   ignore("ghoneycutt-xinetd.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/ghoneycutt-xinetd.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/ghoneycutt-xinetd.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(g) == false)
   }
 
   //not in parser-tests yet
   ignore("thias-ntp.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/thias-ntp.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/thias-ntp.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(g) == false)
   }
 
   //not in parser-tests yet
   ignore("xdrum-rsyslog.pp") {
-    val rg = toGraph(eval(expandAll(Parser.parseFile(s"$root/xdrum-rsyslog.pp"))))
-    val g = toFileScriptGraph(rg)
+    val g = parseFile(s"$root/xdrum-rsyslog.pp").eval.resourceGraph.fsGraph
     assert(SymbolicEvaluator.isDeterministic(g) == false)
   }
 }
