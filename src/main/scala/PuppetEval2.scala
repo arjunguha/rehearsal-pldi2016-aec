0f25aa7fbb35008dbd3302bc1eb00c286b1a5558
jcollard <josephmcollard@gmail.com>
2015-10-09 09:58:58 -0400

    Ensure all resources have a 'stage'. Added a stages map to the State. Preliminary work for adding edges for stages.

--- src/main/scala/PuppetEval2.scala
+++ src/main/scala/PuppetEval2.scala
@@ -76,7 +76,8 @@ object PuppetEval2 {
                    deps: Graph[Node, DiEdge],
                    env: Env,
                    definedTypes: Map[String, VDefinedType],
-                   classes: Map[String, VClass])
+                   classes: Map[String, VClass],
+                   stages: Map[String, Set[Node]])
 
 
 
@@ -106,9 +107,14 @@ object PuppetEval2 {
         (st.copy(deps = st.deps + node), List(node))
       }
       case ResourceDecl(typ, lst) => {
-        val vals = lst.map { case (titleExpr, attrs) =>
-          ResourceVal(typ, evalTitle(st.env, titleExpr),
-                      evalAttrs(st.env, attrs))
+        val vals = lst.map { case (titleExpr, attrs) => {
+          val eas = evalAttrs(st.env, attrs)
+          val attrMap = eas.get("stage") match {
+            case None => eas + ("stage" -> Str("main"))
+            case Some(_) => eas
+          }
+          ResourceVal(typ, evalTitle(st.env, titleExpr), attrMap)
+        }
         }
         val newNodes: Set[Node] = vals.map(_.node).toSet
         //println(s"Creating nodes $nodes")
@@ -119,7 +125,8 @@ object PuppetEval2 {
         else {
           val newResources = vals.map(r => r.node -> r).toMap
           (st.copy(resources = st.resources ++ newResources,
-                   deps = st.deps ++ Graph.from(newNodes, edges = Set())),
+                   deps = st.deps ++ Graph.from(newNodes, edges = Set()),
+                   stages = newResources.foldRight(st.stages)(updateStage)),
            newNodes.toList)
         }
       }
@@ -319,7 +326,24 @@ object PuppetEval2 {
     deps = Graph.empty,
     env = Map("title" -> Str("main"), "name" -> Str("main")),
     definedTypes = Map(),
-    classes = Map())
+    classes = Map(),
+    stages = Map())
+
+  def updateStage(res: (Node, ResourceVal), stages: Map[String, Set[Node]]): Map[String, Set[Node]] = res match {
+    case (node, ResourceVal(_, _, attrMap)) =>
+      attrMap.get("stage") match {
+        case Some(Str(stage)) => addStage(stage, node, stages)
+        case Some(stage) => throw EvalError(s"Stage evaluated to non-string. $stage")
+        case None => throw EvalError(s"No stage defined in $res.")
+      }
+  }
+
+  def addStage(stage: String, node: Node, stages: Map[String, Set[Node]]): Map[String, Set[Node]] =
+      stages.get(stage) match {
+        case None => stages + (stage -> Set(node))
+        case Some(set) => stages + (stage -> (set + node))
+      }
+
 
   def eval(manifest: Manifest): (Map[Node, ResourceVal], Graph[Node, DiEdge]) = {
     val st = evalLoop(evalManifest(emptyState, manifest))
