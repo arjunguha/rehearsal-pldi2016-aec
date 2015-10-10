1003f4429473df9b3dfc419ea072ada348a33c1b
rolivia <rian@cs.umass.edu>
2015-10-10 19:23:59 -0400

    implement sliceGraph

--- src/main/scala/Slicing2.scala
+++ src/main/scala/Slicing2.scala
@@ -6,6 +6,8 @@ object Slicing2 {
   import java.nio.file.Path
   import Implicits._
   import FSSyntax._
+  import scalax.collection.Graph
+  import scalax.collection.GraphPredef._
 
   def allPathsPred(p: Pred): Set[Path] = p match {
     case True | False => Set()
@@ -15,7 +17,6 @@ object Slicing2 {
     case TestFileState(p, _) => Set(p)
     case ITE(a, b, c) => allPathsPred(a) union allPathsPred(b) union allPathsPred(c)
   }
-  
 
   def allPaths(e: Expr, paths: Set[Path]): Set[Path] = e match {
     case Error | Skip => Set()
@@ -30,9 +31,17 @@ object Slicing2 {
     case If(a, p, q) => allPathsPred(a) union allPaths(p, paths) union allPaths(q, paths)
   }
   
-  def slice(paths: Set[Path], e: Expr): Expr = 
+  def slice(e: Expr, paths: Set[Path]): Expr = 
     if((allPaths(e, paths) intersect paths) == Set.empty) Skip
     else e
   
+  def sliceGraph(g: FileScriptGraph): FileScriptGraph = { Graph()
+    //TODO: (Rian) Get set of all paths that appear in more than one node of G
+    val paths: Set[Path] = Set()
+    val nodes = g.nodes.map(p => slice(p, paths))
+    val edges = g.edges.map(e => slice(e.from, paths) ~> slice(e.to, paths))
+    Graph.from(nodes, edges)
+  }
 
+  
 }
