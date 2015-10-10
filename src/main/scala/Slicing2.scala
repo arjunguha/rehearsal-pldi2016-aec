ccd7b26f975a399fce27ea6ba860f87e4e1f7ef3
rolivia <rian@cs.umass.edu>
2015-10-10 18:57:14 -0400

    implemented allPathsPred

--- src/main/scala/Slicing2.scala
+++ src/main/scala/Slicing2.scala
@@ -7,7 +7,15 @@ object Slicing2 {
   import Implicits._
   import FSSyntax._
 
-  def allPathsPred(p: Pred, paths: Set[Path]): Set[Path] = Set()
+  def allPathsPred(p: Pred): Set[Path] = p match {
+    case True | False => Set()
+    case And(a, b) => allPathsPred(a) union allPathsPred(b)
+    case Or(a, b) => allPathsPred(a) union allPathsPred(b)
+    case Not(a) => allPathsPred(a)
+    case TestFileState(p, _) => Set(p)
+    case ITE(a, b, c) => allPathsPred(a) union allPathsPred(b) union allPathsPred(c)
+  }
+  
 
   def allPaths(e: Expr, paths: Set[Path]): Set[Path] = e match {
     case Error | Skip => Set()
@@ -19,7 +27,7 @@ object Slicing2 {
     }
     case Cp(src, dst) => Set(src, dst, dst.getParent())
     case Seq(p, q) => allPaths(p, paths) union allPaths(q, paths)
-    case If(a, p, q) => allPathsPred(a, paths) union allPaths(p, paths) union allPaths(q, paths)
+    case If(a, p, q) => allPathsPred(a) union allPaths(p, paths) union allPaths(q, paths)
   }
   
   def slice(paths: Set[Path], e: Expr): Expr = 
