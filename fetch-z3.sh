9d622b1953c159f6d6d59b2e98231ea576941f42
Arjun Guha <arjun@cs.umass.edu>
2015-10-12 06:04:17 -0400

    deps is taken? use mydeps

---
+++ fetch-z3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -d "/home/travis/mydeps" ]; then
+  echo "/home/travis/mydeps exists, so not fetching any dependencies"
+  exit 0
+fi
+
+cd /home/travis
+mkdir mydeps
+cd mydeps
+
+ZIPFILE=z3-4.4.1-x64-ubuntu-14.04.zip
+URL=https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/$ZIPFILE
+wget $URL
+unzip $ZIPFILE
+mv z3-4.4.1-x64-ubuntu-14.04 z3
+
+git clone https://github.com/regb/scala-smtlib.git
+cd scala-smtlib
+git checkout 711e9a1ef994935482bc83ff3795a94f637f0a04
+sbt publish-local
+
