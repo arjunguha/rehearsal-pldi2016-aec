a306a38e96a6dd4420d0c259d8c588a736d7419a
rolivia <rian@cs.umass.edu>
2015-10-12 11:41:04 -0400

    simplify Rm case in allPaths

--- src/main/scala/Slicing.scala
+++ src/main/scala/Slicing.scala
@@ -22,10 +22,7 @@ object Slicing {
     case Error | Skip => Set()
     case Mkdir(p) => Set(p, p.getParent())
     case CreateFile(p, _) => Set(p, p.getParent())
-    case Rm(f) => {
-      val descendants = paths.filter(p => p != f && p.startsWith(f))
-      descendants union Set(f)
-    }
+    case Rm(f) => paths.filter(p => p.startsWith(f))
     case Cp(src, dst) => Set(src, dst, dst.getParent())
     case Seq(p, q) => allPaths(p, paths) union allPaths(q, paths)
     case If(a, p, q) => allPathsPred(a) union allPaths(p, paths) union allPaths(q, paths)
