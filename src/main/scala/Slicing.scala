d077ad516465cdab710c6783cc9951345ae8290d
rolivia <rian@cs.umass.edu>
2015-10-15 15:21:21 -0400

    fixed(?) slicing to be less conservative

--- src/main/scala/Slicing.scala
+++ src/main/scala/Slicing.scala
@@ -10,29 +10,32 @@ object Slicing {
   import scalax.collection.Graph
   import scalax.collection.GraphPredef._
 
-  def allPathsPred(p: Pred): Set[Path] = p match {
-    case True | False => Set()
-    case And(a, b) => allPathsPred(a) union allPathsPred(b)
-    case Or(a, b) => allPathsPred(a) union allPathsPred(b)
-    case Not(a) => allPathsPred(a)
-    case TestFileState(p, _) => Set(p)
-    case ITE(a, b, c) => allPathsPred(a) union allPathsPred(b) union allPathsPred(c)
-  }
+  def slice(e: Expr, paths: Set[Path]): Expr = e match {
+    case Skip | Error => e
+    case Mkdir(p) => if (paths.contains(p) || paths.contains(p.getParent)) e
+                     else Skip
+    case CreateFile(p, _) => if (paths.contains(p) || paths.contains(p.getParent)) e
+                             else Skip
+    case Rm(p) => if(paths.contains(p) || paths.exists(_.startsWith(p))) e
+                  else Skip
+    case Cp(p1, p2) => 
+      if(paths.contains(p1) || paths.contains(p2) || paths.contains(p2.getParent)) e
+      else Skip
+    case Seq(p, q) =>{ 
+      val pSliced = slice(p, paths)
+      val qSliced = slice(q, paths)
+      if(pSliced == Skip) qSliced
+      else if(qSliced == Skip) pSliced
+      else Seq(pSliced, qSliced)
+    }
+    case If(a, p, q) => {
+      val pSliced = slice(p, paths)
+      val qSliced = slice(q, paths)
+      if(pSliced == qSliced) pSliced
+      else                   If(a, pSliced, qSliced)
+    }
+  }  
 
-  def allPaths(e: Expr, paths: Set[Path]): Set[Path] = e match {
-    case Error | Skip => Set()
-    case Mkdir(p) => Set(p, p.getParent())
-    case CreateFile(p, _) => Set(p, p.getParent())
-    case Rm(f) => paths.filter(p => p.startsWith(f))
-    case Cp(src, dst) => Set(src, dst, dst.getParent())
-    case Seq(p, q) => allPaths(p, paths) union allPaths(q, paths)
-    case If(a, p, q) => allPathsPred(a) union allPaths(p, paths) union allPaths(q, paths)
-  }
-  
-  def slice(e: Expr, paths: Set[Path]): Expr = 
-    if((allPaths(e, paths) intersect paths) == Set.empty) Skip
-    else e
-  
   def interferingPaths(exprs: List[Expr]): Set[Path] = {
     val allPaths = exprs.map(e => Helpers.exprPaths(e))
     val counts = allPaths.flatten.groupBy(identity).mapValues(_.length)
