c141d30e4e03f34c789c7dc2a315b1332d461841
Arjun Guha <arjun@cs.umass.edu>
2015-11-10 19:59:52 -0500

    unused function

--- src/main/scala/SymbolicEvaluator.scala
+++ src/main/scala/SymbolicEvaluator.scala
@@ -383,11 +383,6 @@ class SymbolicEvaluatorImpl(allPaths: List[Path],
     }
   }
 
-  def stXorErr(st1: ST, st2: ST): Term = {
-    st1.isErr && Not(st2.isErr) ||
-    Not(st1.isErr) && st2.isErr
-  }
-
   def stNEq(st1: ST, st2: ST): Term = {
     (st1.isErr && Not(st2.isErr)) ||
       (st2.isErr && Not(st1.isErr)) ||
