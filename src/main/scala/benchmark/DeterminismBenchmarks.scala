1bd7dbf6a1e36b8de6a8376073937c5a0fdd5d68
Aaron Weiss <aaronweiss74@gmail.com>
2015-11-14 16:35:49 -0500

    Modified DeterminismBenchmarks to use a timeout of fifteen minutes.

--- src/main/scala/benchmark/DeterminismBenchmarks.scala
+++ src/main/scala/benchmark/DeterminismBenchmarks.scala
@@ -6,43 +6,47 @@ object DeterminismBenchmarks {
 
   import PuppetSyntax.{FileScriptGraph}
 
-  def bench(label: String, path: String, check: FileScriptGraph => Boolean, onlySliced: Boolean = false, os: String = "ubuntu-trusty"): Unit = {
+  def bench(label: String, path: String, check: FileScriptGraph => Option[Boolean], expected: Boolean, onlySliced: Boolean = false, os: String = "ubuntu-trusty"): Unit = {
     val g = PuppetParser.parseFile(path).eval.resourceGraph.fsGraph(os)
 
+    def checkAndOutput(res: (Option[Boolean], Long), pruning: String, size: Int) = res match {
+      case (Some(out), t) => {
+        assert(out == expected, s"unexpected result from $label with $pruning")
+        println(s"$label,no-pruning,$size,$t")
+      }
+      case (None, _) => println(s"$label,$pruning,$size,timedout")
+    }
+
     if (!onlySliced) {
-      val (r, t) = time(check(g))
-      assert(r == true, s"unexpected result from $label without pruning")
-      val size = g.size
-      println(s"$label,no-pruning,$size,$t")
+      checkAndOutput(time(check(g)), "no-pruning", g.size)
     }
 
     val gPruned = g.pruneWrites()
-    val (r, t) = time(check(gPruned))
-    assert(r == true, s"unexpected result from $label with pruning")
-    val size = gPruned.size
-    println(s"$label,pruning,$size,$t")
+    checkAndOutput(time(check(gPruned)), "pruning", gPruned.size)
   }
 
 
   val trials = 1
   val root = "parser-tests/good"
 
+  def checkWithTimeout(g: FileScriptGraph) = isDeterministicWithTimeout(g, 15 * 60)
+
   def run(): Unit = {
     println("Name,Pruning,Size,Time")
     for (i <- 0.until(trials)) {
-      bench("monit", s"$root/dhoppe-monit.pp", g => isDeterministic(g) == true)
-      bench("bind", s"$root/thias-bind.pp", g => isDeterministic(g) == true)
-      bench("hosting", s"$root/puppet-hosting-fixed.pp", g => isDeterministic(g) == true)
-      bench("dns", s"$root/antonlindstrom-powerdns.pp", g => isDeterministic(g) == false)
-      // bench("irc", s"$root/spiky-reduced.pp", g => isDeterministic(g) == false, onlySliced = true, os = "centos-6")
-      bench("xinetd", s"$root/ghoneycutt-xinetd.pp", g => isDeterministic(g) == false)
-      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", g => isDeterministic(g) == true, os = "centos-6")
-      bench("ntp", s"$root/thias-ntp.pp", g => isDeterministic(g) == false)
-      bench("rsyslog", s"$root/xdrum-rsyslog.pp", g => isDeterministic(g) == false)
-      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", g => isDeterministic(g) == true)
-      bench("amavis", s"$root/mjhas-amavis.pp", g => isDeterministic(g) == true)
-      bench("clamav", s"$root/mjhas-clamav.pp", g => isDeterministic(g) == true)
-      bench("logstash", s"$root/Nelmo-logstash.pp", g => isDeterministic(g) == false)
+      bench("monit", s"$root/dhoppe-monit.pp", checkWithTimeout, true)
+      bench("bind", s"$root/thias-bind.pp", checkWithTimeout, true)
+      bench("hosting", s"$root/puppet-hosting-fixed.pp", checkWithTimeout, true)
+      bench("dns", s"$root/antonlindstrom-powerdns.pp", checkWithTimeout, false)
+      bench("irc", s"$root/spiky-reduced.pp", checkWithTimeout, false, onlySliced = true, os = "centos-6")
+      bench("xinetd", s"$root/ghoneycutt-xinetd.pp", checkWithTimeout, false)
+      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", checkWithTimeout, true, os = "centos-6")
+      bench("ntp", s"$root/thias-ntp.pp", checkWithTimeout, false)
+      bench("rsyslog", s"$root/xdrum-rsyslog.pp", checkWithTimeout, false)
+      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", checkWithTimeout, true)
+      bench("amavis", s"$root/mjhas-amavis.pp", checkWithTimeout, true)
+      bench("clamav", s"$root/mjhas-clamav.pp", checkWithTimeout, true)
+      bench("logstash", s"$root/Nelmo-logstash.pp", checkWithTimeout, false)
     }
   }
 
