2d2ed8c7d38d45a0fffc7a505a599e56cd971a52
Aaron Weiss <aaronweiss74@gmail.com>
2015-11-17 14:58:37 -0500

    Added pruned size to IndempotenceSizeTable.

--- src/main/scala/benchmark/IdempotenceSizeTable.scala
+++ src/main/scala/benchmark/IdempotenceSizeTable.scala
@@ -9,7 +9,8 @@ object IdempotenceSizeTables {
     val g = rg.fsGraph(os)
     val rSize = rg.deps.nodes.size
     val fSize =  g.size
-    println(s"$label & $rSize & $fSize \\\\")
+    val fPrunedSize = g.expr.pruneIdem.size
+    println(s"$label & $rSize & $fSize & $fPrunedSize \\\\")
   }
 
 
