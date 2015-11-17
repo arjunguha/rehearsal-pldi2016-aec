f9e09672d8a5f7958aa6579caca65e6afedce25c
rolivia <rian@cs.umass.edu>
2015-11-17 15:37:00 -0500

    now runs both pruned and not pruned versions

--- src/main/scala/benchmark/IdempotenceBenchmarks.scala
+++ src/main/scala/benchmark/IdempotenceBenchmarks.scala
@@ -7,14 +7,19 @@ object IdempotenceBenchmarks {
   import PuppetSyntax.{FileScriptGraph}
   import FSSyntax.{Expr}
 
-  def bench(label: String, path: String, check: Expr => Boolean, os: String = "ubuntu-trusty"): Unit = {
+  def bench(label: String, path: String, expected: Boolean, os: String = "ubuntu-trusty"): Unit = {
     val g = PuppetParser.parseFile(path).eval.resourceGraph.fsGraph(os)
 
+    def checkAndOutput(res: (Boolean, Long), pruning: String, size: Int) = res match {
+      case (out, t) => {
+        assert(out == expected, s"unexpected result from $label with $pruning")
+        println(s"$label,$pruning,$size,$t")
+      }
+    }
+
+    checkAndOutput(time(isIdempotent(g)), "no-pruning", g.size)
     val gPruned = g.expr().pruneIdem()
-    val (r, t) = time(check(gPruned))
-    assert(r == true, s"unexpected result from $label with pruning")
-    val size = gPruned.size
-    println(s"$label,pruning,$size,$t")
+    checkAndOutput(time(gPruned.isIdempotent), "pruning", gPruned.size)
   }
 
 
@@ -24,19 +29,19 @@ object IdempotenceBenchmarks {
   def run(): Unit = {
     println("Name,Pruning,Size,Time")
     for (i <- 0.until(trials)) {
-      bench("monit", s"$root/dhoppe-monit.pp", g => g.isIdempotent == true)
-      bench("bind", s"$root/thias-bind.pp", g => g.isIdempotent == true)
-      bench("hosting", s"$root/puppet-hosting_deter.pp", g => g.isIdempotent == true)
-      bench("dns", s"$root/antonlindstrom-powerdns_deter.pp", g => g.isIdempotent == true)
-      bench("irc", s"$root/spiky-reduced-deterministic.pp", g => g.isIdempotent == true, os = "centos-6")
-      bench("xinetd", s"$root/ghoneycutt-xinetd_deter.pp", g => g.isIdempotent == true)
-      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", g => g.isIdempotent == true, os = "centos-6")
-      bench("ntp", s"$root/thias-ntp_deter.pp", g => g.isIdempotent == true)
-      bench("rsyslog", s"$root/xdrum-rsyslog_deter.pp", g => g.isIdempotent == true)
-      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", g => g.isIdempotent == true)
-      bench("amavis", s"$root/mjhas-amavis.pp", g => g.isIdempotent == true)
-      bench("clamav", s"$root/mjhas-clamav.pp", g => g.isIdempotent == true)
-      bench("logstash", s"$root/Nelmo-logstash_deter.pp", g => g.isIdempotent == true)
+      bench("monit", s"$root/dhoppe-monit.pp", true)
+      bench("bind", s"$root/thias-bind.pp", true)
+      bench("hosting", s"$root/puppet-hosting_deter.pp", true)
+      bench("dns", s"$root/antonlindstrom-powerdns_deter.pp", true)
+      bench("irc", s"$root/spiky-reduced-deterministic.pp", true, os = "centos-6")
+      bench("xinetd", s"$root/ghoneycutt-xinetd_deter.pp", true)
+      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", true, os = "centos-6")
+      bench("ntp", s"$root/thias-ntp_deter.pp", true)
+      bench("rsyslog", s"$root/xdrum-rsyslog_deter.pp", true)
+      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", true)
+      bench("amavis", s"$root/mjhas-amavis.pp", true)
+      bench("clamav", s"$root/mjhas-clamav.pp", true)
+      bench("logstash", s"$root/Nelmo-logstash_deter.pp", true)
     }
   }
 
