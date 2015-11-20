87d129e9ceeaeb45c0f48c1ad54ad14650ae92eb
Arjun Guha <arjun@cs.umass.edu>
2015-11-20 04:56:51 -0800

    bench

--- src/main/scala/benchmark/DeterminismBenchmarks.scala
+++ src/main/scala/benchmark/DeterminismBenchmarks.scala
@@ -6,13 +6,34 @@ object DeterminismBenchmarks {
 
   import PuppetSyntax.{FileScriptGraph}
 
-  def bench(label: String, path: String, check: FileScriptGraph => Boolean, onlySliced: Boolean = false, os: String = "ubuntu-trusty"): Unit = {
+  def check(args: Seq[String]): Boolean = {
+    import scala.concurrent._
+    import ExecutionContext.Implicits.global
+    import scala.sys.process._
+
+    val p = args.run()
+    val f = Future(blocking(p.exitValue())) // wrap in Future
+    try {
+        Await.result(f, duration.Duration(600, "sec"))
+        true
+      } catch {
+        case _: TimeoutException =>
+          p.destroy()
+          false
+      }
+  }
+
+  def bench(label: String, path: String, onlySliced: Boolean = false, os: String = "ubuntu-trusty"): Unit = {
     val g = PuppetParser.parseFile(path).eval.resourceGraph.fsGraph(os)
     import scala.sys.process._
 
-    Seq("./run.sh", "deterbench", label, path, os, "prune").!
+    if (check(Seq("./run.sh", "deterbench", label, path, os, "prune")) == false) {
+      println(s"$label, pruning, 0, 0")
+    }
     if (onlySliced != true) {
-      Seq("./run.sh", "deterbench", label, path, os, "noprune").!
+      if (check(Seq("./run.sh", "deterbench", label, path, os, "noprune")) == false) {
+        println(s"$label, no-pruning, 0, 0")
+      }
     }
   }
 
@@ -32,22 +53,24 @@ object DeterminismBenchmarks {
   }
 
 
+
+
   def run(): Unit = {
     println("Name,Pruning,Size,Time")
     for (i <- 0.until(trials)) {
-      bench("monit", s"$root/dhoppe-monit.pp", g => isDeterministic(g) == true)
-      bench("bind", s"$root/thias-bind.pp", g => isDeterministic(g) == true)
-      bench("hosting", s"$root/puppet-hosting-fixed.pp", g => isDeterministic(g) == true)
-      bench("dns", s"$root/antonlindstrom-powerdns.pp", g => isDeterministic(g) == false)
-      // bench("irc", s"$root/spiky-reduced.pp", g => isDeterministic(g) == false, onlySliced = true, os = "centos-6")
-      bench("xinetd", s"$root/ghoneycutt-xinetd.pp", g => isDeterministic(g) == false)
-      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", g => isDeterministic(g) == true, os = "centos-6")
-      bench("ntp", s"$root/thias-ntp.pp", g => isDeterministic(g) == false)
-      bench("rsyslog", s"$root/xdrum-rsyslog.pp", g => isDeterministic(g) == false)
-      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", g => isDeterministic(g) == true)
-      bench("amavis", s"$root/mjhas-amavis.pp", g => isDeterministic(g) == true)
-      bench("clamav", s"$root/mjhas-clamav.pp", g => isDeterministic(g) == true)
-      bench("logstash", s"$root/Nelmo-logstash.pp", g => isDeterministic(g) == false)
+      bench("monit", s"$root/dhoppe-monit.pp")
+      bench("bind", s"$root/thias-bind.pp")
+      bench("hosting", s"$root/puppet-hosting_fixed.pp")
+      bench("dns", s"$root/antonlindstrom-powerdns.pp")
+      bench("irc", s"$root/spiky-reduced.pp",  os = "centos-6")
+      bench("xinetd", s"$root/ghoneycutt-xinetd.pp")
+      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", os = "centos-6")
+      bench("ntp", s"$root/thias-ntp.pp")
+      bench("rsyslog", s"$root/xdrum-rsyslog.pp")
+      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp")
+      bench("amavis", s"$root/mjhas-amavis.pp")
+      bench("clamav", s"$root/mjhas-clamav.pp")
+      bench("logstash", s"$root/Nelmo-logstash.pp")
     }
   }
 
