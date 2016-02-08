2a2f85a2600488c0ac79dfef4ea741e873964aeb
Arjun Guha <arjun@cs.umass.edu>
2016-02-08 14:28:42 -0500

    Optimizations to DeterminismPruning2
    - Reuses SAT-solver state better.
    - Cheap hack to detect paths that are obviously files.

--- src/main/scala/DeterminismPruning2.scala
+++ src/main/scala/DeterminismPruning2.scala
@@ -1,6 +1,6 @@
 package rehearsal
 
-object DeterminismPruning2 {
+object DeterminismPruning2 extends com.typesafe.scalalogging.LazyLogging {
 
   import PuppetSyntax.FSGraph
   import Implicits._
@@ -30,11 +30,55 @@ object DeterminismPruning2 {
     sym.everSucceeds(test(IsDir)) ^ sym.everSucceeds(test(IsFile))
   }
 
+  def isTriviallyFile(path: Path, expr: Expr): Boolean = expr match {
+    case CreateFile(_, _) => true
+    case Mkdir(p) => path != p
+    case Cp(src, _) => path != src
+    case Rm(p) => path != p
+    case Seq(e1, e2) => isTriviallyFile(path, e1) && isTriviallyFile(path, e2)
+    case If(pred, e1, e2) => !pred.readSet.contains(path) && isTriviallyFile(path, e1) && isTriviallyFile(path, e2)
+    case Skip => true
+    case Error => true
+  }
+
   def pruneWrites[K](graph: FSGraph[K]): FSGraph[K] = {
+    import smtlib._
+    import parser._
+    import Commands._
+    import Terms._
+    import theories.Core.{And => _, Or => _, _}
+
     val candidates = pruningCandidates(graph.exprs).map({
       case (k, paths) => {
+        logger.info(s"${paths.size} candidate paths for $k")
+
         val e = graph.exprs(k)
-        (k, paths.filter(p => isDefinitiveWrite(p, e)))
+        val sym = new SymbolicEvaluatorImpl(e.paths.toList, e.hashes,
+          Set(), None)
+        val initSt = sym.initState
+        sym.smt.eval(Assert(Not(initSt.isErr)))
+        sym.assertPathConsistency(initSt)
+        val interSt = sym.evalExpr(initSt, e)
+
+        val (files, otherPaths) = paths.partition(p => isTriviallyFile(p, e))
+
+        val validPaths = otherPaths.filter { path =>
+
+          def test(stat: FileState): Expr = {
+            ite(testFileState(path, stat), Skip, Error)
+          }
+
+          val everDir = sym.smt.pushPop {
+            sym.smt.eval(Assert(Not(sym.evalExpr(interSt, test(IsDir)).isErr)))
+            sym.smt.checkSat()
+          }
+          val everFile = sym.smt.pushPop {
+            sym.smt.eval(Assert(Not(sym.evalExpr(interSt, test(IsDir)).isErr)))
+            sym.smt.checkSat()
+          }
+          everDir ^ everFile
+        }
+        (k, files ++ validPaths)
       }
     }).toMap
 
