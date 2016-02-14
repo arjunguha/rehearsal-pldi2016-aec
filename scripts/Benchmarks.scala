7a39b0db978682aa29c15f23f94074aa82de7ddf
Arjun Guha <arjun@cs.umass.edu>
2016-02-14 10:20:42 -0500

    Benchmarking
    - Copied R scripts over from paper repository.
    - We now run each benchmark in a separate process. Prevents cached stuff
      from an earlier benchmark from affecting current benchmark.

---
+++ scripts/Benchmarks.scala
@@ -0,0 +1,146 @@
+import scala.sys.process._
+import java.nio.file._
+
+val root = "parser-tests/good"
+
+abstract class Benchmark {
+
+  val output = new collection.mutable.StringBuilder()
+
+  private val logger = ProcessLogger(
+    line => {
+      println(line)
+      output ++= line
+      output += '\n'
+    },
+    line => println(s"ERROR: $line"))
+
+  type Command
+
+  def run(commandVal: Command): Unit = {
+    val proc = Seq("sbt", "-J-Xmx4G",
+      "-Dorg.slf4j.simpleLogger.defaultLogLevel=info",
+      "-Dorg.slf4j.simpleLogger.logFile=rehearsal.log",
+      "--warn", "set showSuccess in ThisBuild := false",
+      commandVal.toString)
+      .run(logger)
+    assert(proc.exitValue == 0)
+  }
+
+}
+
+def doSizes(output: String): Unit = {
+  val sizes = new Benchmark {
+
+    class Command(label: String, filename: String, os: String) {
+      override def toString(): String = {
+        s"run benchmark-pruning-size --filename $filename --label $label --os $os"
+      }
+    }
+
+    def bench(label: String, filename: String, os: String = "ubuntu-trusty") = {
+      run(new Command(label, filename, os))
+    }
+
+    output ++= "Name, Before, After\n"
+    bench("monit", s"$root/dhoppe-monit.pp")
+    bench("bind", s"$root/thias-bind.pp")
+    bench("hosting", s"$root/puppet-hosting_deter.pp")
+    bench("dns", s"$root/antonlindstrom-powerdns.pp")
+    bench("irc", s"$root/nfisher-SpikyIRC.pp", os = "centos-6")
+    bench("xinetd", s"$root/ghoneycutt-xinetd.pp")
+    bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", os = "centos-6")
+    bench("ntp", s"$root/thias-ntp.pp")
+    bench("rsyslog", s"$root/xdrum-rsyslog.pp")
+    bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp")
+    bench("amavis", s"$root/mjhas-amavis.pp")
+    bench("clamav", s"$root/mjhas-clamav.pp")
+    bench("logstash", s"$root/Nelmo-logstash.pp")
+  }
+
+  Files.write(Paths.get(output), sizes.output.toString.getBytes)
+}
+
+def doDeterminism(trials: Int, output: String): Unit = {
+  val determinism = new Benchmark {
+
+    class Command(label: String, filename: String, os: String,
+                  pruning: Boolean,
+                  deterministic: Boolean) {
+      override def toString(): String = {
+        s"run benchmark-determinism --filename $filename --label $label --os $os --pruning $pruning --deterministic $deterministic"
+      }
+    }
+
+    def bench(label: String, filename: String, deterministic: Boolean,
+              onlyPruning: Boolean = false,
+               os: String = "ubuntu-trusty") = {
+      run(new Command(label, filename, os, true, deterministic))
+      if (onlyPruning == false) {
+        run(new Command(label, filename, os, false, deterministic))
+      }
+    }
+
+    output ++= "Name, Pruning, Time\n"
+    for (i <- 0.until(trials)) {
+      bench("monit", s"$root/dhoppe-monit.pp", true)
+      bench("bind", s"$root/thias-bind.pp", true)
+      bench("hosting", s"$root/puppet-hosting_deter.pp", true)
+      bench("dns", s"$root/antonlindstrom-powerdns.pp", false)
+      bench("irc", s"$root/nfisher-SpikyIRC.pp", false, onlyPruning = true, os = "centos-6")
+      bench("xinetd", s"$root/ghoneycutt-xinetd.pp", false)
+      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", true, os = "centos-6")
+      bench("ntp", s"$root/thias-ntp.pp", false)
+      bench("rsyslog", s"$root/xdrum-rsyslog.pp", false)
+      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", true)
+      bench("amavis", s"$root/mjhas-amavis.pp", true)
+      bench("clamav", s"$root/mjhas-clamav.pp", true)
+      bench("logstash", s"$root/Nelmo-logstash.pp", false)
+    }
+  }
+
+  Files.write(Paths.get(output), determinism.output.toString.getBytes)
+}
+
+def doIdempotence(trials: Int, output: String): Unit = {
+  val idempotence = new Benchmark {
+
+    class Command(label: String, filename: String, os: String,
+                  idempotent: Boolean) {
+      override def toString(): String = {
+        s"run benchmark-idempotence --filename $filename --label $label --os $os --idempotent $idempotent"
+      }
+    }
+
+    def bench(label: String, filename: String, idempotent: Boolean,
+               os: String = "ubuntu-trusty") = {
+      run(new Command(label, filename, os, idempotent))
+    }
+
+    output ++= "Name, Time\n"
+    for (i <- 0.until(trials)) {
+      bench("monit", s"$root/dhoppe-monit.pp", true)
+      bench("bind", s"$root/thias-bind.pp", true)
+      bench("hosting", s"$root/puppet-hosting_deter.pp", true)
+      bench("dns", s"$root/antonlindstrom-powerdns_deter.pp", true)
+      bench("irc", s"$root/spiky-reduced-deterministic.pp", true, os = "centos-6")
+      bench("xinetd", s"$root/ghoneycutt-xinetd_deter.pp", true)
+      bench("jpa", s"$root/pdurbin-java-jpa-tutorial.pp", true, os = "centos-6")
+      bench("ntp", s"$root/thias-ntp_deter.pp", true)
+      bench("rsyslog", s"$root/xdrum-rsyslog_deter.pp", true)
+      bench("nginx", s"$root/BenoitCattie-puppet-nginx.pp", true)
+      bench("amavis", s"$root/mjhas-amavis.pp", true)
+      bench("clamav", s"$root/mjhas-clamav.pp", true)
+      bench("logstash", s"$root/Nelmo-logstash_deter.pp", true)
+    }
+  }
+
+  Files.write(Paths.get(output), idempotence.output.toString.getBytes)
+}
+
+args match {
+  case Array("sizes", output) => doSizes(output)
+  case Array("determinism", n, output) => doDeterminism(n.toInt, output)
+  case Array("idempotence", n, output) => doIdempotence(n.toInt, output)
+}
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,46 +1,133 @@
 package rehearsal
 
 object Main extends App {
-  
-  def runOnAll[A](path: String, noun: String, pastVerb: String)(action: String => A) {
-    import java.io.File
-    import scala.util.{Try, Success, Failure}
-    def walkTree(file: File): Iterable[File] = {
-      val children = new Iterable[File] {
-        def iterator = if (file.isDirectory) file.listFiles.iterator else Iterator.empty
-      }
-      Seq(file) ++: children.flatMap(walkTree(_))
+
+  import scala.concurrent._
+  import scala.concurrent.duration._
+  import ExecutionContext.Implicits.global
+  import Implicits._
+
+  sealed trait Command
+  case object DeterminismBenchmark extends Command
+  case object PruningSizeBenchmark extends Command
+  case object IdempotenceBenchmark extends Command
+
+  case class Config(
+    command: Option[Command],
+    filename: Option[Path],
+    os: Option[String],
+    label: Option[String],
+    pruning: Option[Boolean],
+    deterministic: Option[Boolean],
+    idempotent: Option[Boolean])
+
+  val parser = new scopt.OptionParser[Config]("scopt") {
+
+    def filename() = {
+      opt[String]("filename").required
+        .action((x, c)  => c.copy(filename = Some(x.toPath)))
     }
-    var count = 0
-    var total = 0
-    println(s"Running $noun on all Puppet files within $path.")
-    walkTree(new File(path)).filter(_.getPath().endsWith(".pp")) foreach { file =>
-      total += 1
-      if (Try(action(file.getPath())).isSuccess) {
-        println(s"Successfully $pastVerb ${file.getPath()}.")
-        count += 1
-      }
+
+    def label() = {
+      opt[String]("label").required
+        .action((x, c) => c.copy(label = Some(x)))
     }
-    println(s"${pastVerb.capitalize} $count files out of $total. (${(count * 100) / total}%)")
+
+    def os() = {
+      opt[String]("os").required
+        .action((x, c) => c.copy(os = Some(x)))
+    }
+
+    head("rehearsal", "0.1")
+
+    cmd("benchmark-pruning-size")
+      .action((_, c) => c.copy(command = Some(PruningSizeBenchmark)))
+      .text("Benchmark the effect of pruning on the number of writable paths")
+      .children(filename, label, os)
+
+    cmd("benchmark-idempotence")
+      .action((_, c) => c.copy(command = Some(IdempotenceBenchmark)))
+      .text("Benchmark the performance of idempotence-checking")
+      .children(filename, label, os,
+        opt[Boolean]("idempotent").required
+          .action((x,c) => c.copy(idempotent = Some(x))))
+
+    cmd("benchmark-determinism") action { (_, c) =>
+      c.copy(command = Some(DeterminismBenchmark))
+    } text("Benchmark determinism checking") children(
+      filename,
+      label,
+      os,
+      opt[Boolean]("pruning").required
+        .action((x, c) => c.copy(pruning = Some(x))),
+      opt[Boolean]("deterministic").required
+          .action((x, c) => c.copy(deterministic = Some(x)))
+    )
   }
 
-  def uncurry[A, B, C](f: (A, B) => C)(t: (A, B)): C = f(t._1, t._2)
-
-  args.toList match {
-
-    case List("parseall", path) => runOnAll(path, "parser", "parsed") {
-      PuppetParser.parseFile(_)
+  parser.parse(args, Config(None, None, None, None, None, None, None)) match {
+    case None => {
+      println(parser.usage)
+      System.exit(1)
     }
-    case List("evalall", path) => runOnAll(path, "evaluator", "evaluated") {
-      PuppetParser.parseFile(_).eval
-    }
-    case List("detersuite") => DeterminismBenchmarks.run()
-    case List("detersizes") => DeterminismSizeTables.run()
-    case List("deterstress") => DeterStressBenchmark.run()
-    case List("idemsuite") => IdempotenceBenchmarks.run()
-    case List("idemsizes") => IdempotenceSizeTables.run()
-    case args => {
-      sys.error(s"Invalid command-line arguments: $args")
+    case Some(config) => {
+      config.command match {
+        case None => {
+          println(parser.usage)
+          System.exit(1)
+        }
+        case Some(IdempotenceBenchmark) => {
+          val filename = config.filename.get
+          val label = config.label.get
+          val os = config.os.get
+          val expected = config.idempotent.get
+          val g = PuppetParser.parseFile(filename.toString).eval.resourceGraph
+            .fsGraph(os)
+            .expr
+            .pruneIdem
+          val (isIdempotent, t) = time(g.isIdempotent)
+          assert (expected == isIdempotent)
+          println(s"$label, $t")
+        }
+        case Some(PruningSizeBenchmark) => {
+          val filename = config.filename.get
+          val label = config.label.get
+          val os = config.os.get
+          val g1 = PuppetParser.parseFile(filename.toString)
+            .eval.resourceGraph.fsGraph(os)
+            .addRoot(PuppetSyntax.Node("root", "root"))
+            .contractEdges()
+          val g2 = g1.pruneWrites()
+          val paths = g1.expr.fileSets.writes.size
+          val postPruningPaths =  g2.pruneWrites.expr.fileSets.writes.size
+          println(s"$label, $paths, $postPruningPaths")
+        }
+        case Some(DeterminismBenchmark) => {
+
+          val label = config.label.get
+          val pruningStr = if (config.pruning.get) "pruning" else "no-pruning"
+          val shouldBeDeterministic = config.deterministic.get
+
+          val g_ = PuppetParser.parseFile(config.filename.get.toString)
+            .eval.resourceGraph.fsGraph(config.os.get)
+            .addRoot(PuppetSyntax.Node("root", "root"))
+            .contractEdges()
+
+          val g = if (config.pruning.get) g_.pruneWrites() else g_
+          try {
+            val (isDeterministic, t) =
+              Await.result(Future(time(SymbolicEvaluator.isDeterministic(g))),
+                           120.seconds)
+              assert (shouldBeDeterministic == isDeterministic)
+              println(s"$label, $pruningStr, $t")
+          }
+          catch {
+            case exn:TimeoutException => {
+              println(s"$label, $pruningStr, timedout")
+            }
+          }
+        }
+      }
     }
   }
 
