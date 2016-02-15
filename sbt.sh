3de55546540115a1fc5f4ef48e714ba1a58e56c4
Arjun Guha <arjun@cs.umass.edu>
2016-02-15 04:25:02 -0500

    Still dont know how to write shell scripts

--- sbt.sh
+++ sbt.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 ARGS=$@
 
-if [[ `uname` -eq "Darwin" ]]; then
-  echo "Logging to ~/Library/Logs/rehearsal.log"
-  LOGFILE=~/Library/Logs/rehearsal.log
-else
-  LOGFILE=rehearsal.log
-fi
+LOGFILE=rehearsal.log
 
 sbt -J-Xmx4G -Dorg.slf4j.simpleLogger.defaultLogLevel=info \
   -Dorg.slf4j.simpleLogger.logFile=$LOGFILE
