e36df56ec6773cc107ea0980150d256e2a0811e9
Arjun Guha <arjun@cs.umass.edu>
2016-04-11 12:43:23 -0400

    Made option parsing smaller

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,83 +9,82 @@ object Main extends App {
   import Implicits._
   import scala.util.{Try, Success, Failure}
 
-  sealed trait Command
-  case object Checker extends Command
-  case object DeterminismBenchmark extends Command
-  case object PruningSizeBenchmark extends Command
-  case object IdempotenceBenchmark extends Command
-  case object ScalabilityBenchmark extends Command
-
   case class Config(
-    command: Option[Command],
-    filename: Option[Path],
-    os: Option[String],
-    label: Option[String],
-    pruning: Option[Boolean],
-    deterministic: Option[Boolean],
-    idempotent: Option[Boolean],
-    size: Option[Int])
+    command: Config => Unit,
+    string: Map[String, String],
+    bool: Map[String, Boolean],
+    int: Map[String, Int])
 
-  val parser = new scopt.OptionParser[Config]("rehearsal") {
 
-    def filename() = {
-      opt[String]("filename").required
-        .action((x, c)  => c.copy(filename = Some(x.toPath)))
-    }
-
-    def label() = {
-      opt[String]("label").required
-        .action((x, c) => c.copy(label = Some(x)))
-    }
-
-    def os() = {
-      opt[String]("os").required
-        .action((x, c) => c.copy(os = Some(x)))
-    }
-
-    def size() = {
-      opt[Int]("size").required.action((x, c) => c.copy(size = Some(x)))
-    }
-
-    head("rehearsal", "0.1")
-
-    cmd("check")
-        .action((_, c) => c.copy(command = Some(Checker)))
-        .text("Check a Puppet manifest for errors.")
-        .children(filename, os)
-
-    cmd("benchmark-pruning-size")
-      .action((_, c) => c.copy(command = Some(PruningSizeBenchmark)))
-      .text("Benchmark the effect of pruning on the number of writable paths")
-      .children(filename, label, os)
-
-    cmd("benchmark-idempotence")
-      .action((_, c) => c.copy(command = Some(IdempotenceBenchmark)))
-      .text("Benchmark the performance of idempotence-checking")
-      .children(filename, label, os,
-        opt[Boolean]("idempotent").required
-          .action((x,c) => c.copy(idempotent = Some(x))))
-
-    cmd("benchmark-determinism") action { (_, c) =>
-      c.copy(command = Some(DeterminismBenchmark))
-    } text("Benchmark determinism checking") children(
-      filename,
-      label,
-      os,
-      opt[Boolean]("pruning").required
-        .action((x, c) => c.copy(pruning = Some(x))),
-      opt[Boolean]("deterministic").required
-          .action((x, c) => c.copy(deterministic = Some(x)))
-    )
-
-    cmd("scalability-benchmark")
-      .action((_, c) => c.copy(command = Some(ScalabilityBenchmark)))
-      .text("Benchmark determinism-checking scaling")
-     .children(size)
-
+  def usage(config: Config): Unit = {
+    println(parser.usage)
+    System.exit(1)
   }
 
-  def checker(filename: String, os: String): Unit = {
+  def pruningSizeBenchmark(config: Config): Unit = {
+    val filename = config.string("filename")
+    val label = config.string("label")
+    val os = config.string("os")
+    val g1 = PuppetParser.parseFile(filename.toString)
+      .eval.resourceGraph.fsGraph(os)
+    val g2 = g1.pruneWrites()
+    val paths = g1.expr.fileSets.writes.size
+    val postPruningPaths =  g2.pruneWrites.expr.fileSets.writes.size
+    println(s"$label, $paths, $postPruningPaths")
+  }
+
+  def idempotenceBenchmark(config: Config): Unit = {
+    val filename = config.string("filename").toPath
+    val label = config.string("label").toPath
+    val os = config.string("os")
+    val expected = config.bool("idempotent")
+    val g = PuppetParser.parseFile(filename.toString).eval.resourceGraph
+      .fsGraph(os)
+      .expr
+    val (isIdempotent, t) = time(g.isIdempotent)
+    assert (expected == isIdempotent)
+    println(s"$label, $t")
+  }
+
+  def determinismBenchmark(config: Config): Unit = {
+    val label = config.string("label")
+    val pruningStr = if (config.bool("pruning")) "pruning" else "no-pruning"
+    val shouldBeDeterministic = config.bool("deterministic")
+
+    val g_ = PuppetParser.parseFile(config.string("filename"))
+      .eval.resourceGraph.fsGraph(config.string("os"))
+
+    val g = if (config.bool("pruning")) g_.pruneWrites() else g_
+    try {
+      val (isDeterministic, t) =
+        Await.result(Future(time(g.toExecTree().isDeterministic())),
+          5.minutes)
+      assert (shouldBeDeterministic == isDeterministic)
+      println(s"$label, $pruningStr, $t")
+    }
+    catch {
+      case exn:TimeoutException => {
+        println(s"$label, $pruningStr, timedout")
+      }
+    }
+  }
+
+  def scalabilityBenchmark(config: Config): Unit = {
+    val n = config.int("size")
+    import ResourceModel.{File, CInline}
+    import scalax.collection.Graph
+    import scalax.collection.GraphEdge.DiEdge
+    val e = ResourceModel.File(path = "/a".toPath, content = CInline("content"), force = false)
+      .compile("ubuntu-trusty")
+    val nodes = 0.until(n).map(n => FSGraph.key())
+    val graph = FSGraph(nodes.map(n => n -> e).toMap, Graph(nodes: _*))
+    val (_, t) = time(graph.pruneWrites().toExecTree().isDeterministic)
+    println(s"$n, $t")
+  }
+
+  def checker(config: Config): Unit = {
+    val os = config.string("os")
+    val filename = config.string("filename").toPath
     if (!Set("ubuntu-trusty", "centos-6").contains(os)) {
       println("""Error: supported operating systems are 'ubuntu-trusty' and 'centos-6'""")
       return
@@ -97,7 +96,7 @@ object Main extends App {
     }
 
     Try(PuppetParser.parseFile(filename.toString)
-          .eval.resourceGraph.fsGraph(os)) match {
+      .eval.resourceGraph.fsGraph(os)) match {
       case Success(g) => {
         print("Checking if manifest is deterministic ... ")
         if (g.pruneWrites().toExecTree().isDeterministic()) {
@@ -132,81 +131,60 @@ object Main extends App {
     }
   }
 
-  parser.parse(args, Config(None, None, None, None, None, None, None, None)) match {
+  val parser = new scopt.OptionParser[Config]("rehearsal") {
+
+    def string(name: String) = {
+      opt[String](name).required
+        .action((x, c)  => c.copy(string = c.string + (name -> x)))
+    }
+
+    def bool(name: String) = {
+      opt[Boolean](name).required
+        .action((x, c)  => c.copy(bool = c.bool + (name -> x)))
+    }
+
+    def int(name: String) = {
+      opt[Int](name).required
+        .action((x, c)  => c.copy(int = c.int + (name -> x)))
+    }
+
+    head("rehearsal", "0.1")
+
+    cmd("check")
+        .action((_, c) => c.copy(command = checker))
+        .text("Check a Puppet manifest for errors.")
+        .children(string("filename"), string("os"))
+
+    cmd("benchmark-pruning-size")
+      .action((_, c) => c.copy(command = pruningSizeBenchmark))
+      .text("Benchmark the effect of pruning on the number of writable paths")
+      .children(string("filename"), string("label"), string("os"))
+
+    cmd("benchmark-idempotence")
+      .action((_, c) => c.copy(command = idempotenceBenchmark))
+      .text("Benchmark the performance of idempotence-checking")
+      .children(string("filename"), string("label"), string("os"), bool("idempotent"))
+
+    cmd("benchmark-determinism")
+      .action((_, c) => c.copy(command = determinismBenchmark))
+      .text("Benchmark determinism checking")
+      .children(string("filename"), string("label"), string("os"),
+        bool("pruning"), bool("deterministic"))
+
+    cmd("scalability-benchmark")
+      .action((_, c) => c.copy(command = scalabilityBenchmark))
+      .text("Benchmark determinism-checking scaling")
+      .children(int("size"))
+
+  }
+
+  parser.parse(args, Config(usage, Map(), Map(), Map())) match {
     case None => {
       println(parser.usage)
       System.exit(1)
     }
     case Some(config) => {
-      config.command match {
-        case None => {
-          println(parser.usage)
-          System.exit(1)
-        }
-        case Some(Checker) => {
-          val filename = config.filename.get
-          val os = config.os.get
-          checker(filename.toString, os)
-        }
-        case Some(IdempotenceBenchmark) => {
-          val filename = config.filename.get
-          val label = config.label.get
-          val os = config.os.get
-          val expected = config.idempotent.get
-          val g = PuppetParser.parseFile(filename.toString).eval.resourceGraph
-            .fsGraph(os)
-            .expr
-          val (isIdempotent, t) = time(g.isIdempotent)
-          assert (expected == isIdempotent)
-          println(s"$label, $t")
-        }
-        case Some(PruningSizeBenchmark) => {
-          val filename = config.filename.get
-          val label = config.label.get
-          val os = config.os.get
-          val g1 = PuppetParser.parseFile(filename.toString)
-            .eval.resourceGraph.fsGraph(os)
-          val g2 = g1.pruneWrites()
-          val paths = g1.expr.fileSets.writes.size
-          val postPruningPaths =  g2.pruneWrites.expr.fileSets.writes.size
-          println(s"$label, $paths, $postPruningPaths")
-        }
-        case Some(DeterminismBenchmark) => {
-
-          val label = config.label.get
-          val pruningStr = if (config.pruning.get) "pruning" else "no-pruning"
-          val shouldBeDeterministic = config.deterministic.get
-
-          val g_ = PuppetParser.parseFile(config.filename.get.toString)
-            .eval.resourceGraph.fsGraph(config.os.get)
-
-          val g = if (config.pruning.get) g_.pruneWrites() else g_
-          try {
-            val (isDeterministic, t) =
-              Await.result(Future(time(g.toExecTree().isDeterministic())),
-                           5.minutes)
-              assert (shouldBeDeterministic == isDeterministic)
-              println(s"$label, $pruningStr, $t")
-          }
-          catch {
-            case exn:TimeoutException => {
-              println(s"$label, $pruningStr, timedout")
-            }
-          }
-        }
-        case Some(ScalabilityBenchmark) => {
-          val n = config.size.get
-          import ResourceModel.{File, CInline}
-          import scalax.collection.Graph
-          import scalax.collection.GraphEdge.DiEdge
-          val e = ResourceModel.File(path = "/a".toPath, content = CInline("content"), force = false)
-            .compile("ubuntu-trusty")
-          val nodes = 0.until(n).map(n => FSGraph.key())
-          val graph = FSGraph(nodes.map(n => n -> e).toMap, Graph(nodes: _*))
-          val (_, t) = time(graph.pruneWrites().toExecTree().isDeterministic)
-          println(s"$n, $t")
-        }
-      }
+      config.command(config)
     }
   }
 
